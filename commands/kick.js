76eab3f2315317750fd48cd2a454893fdd18f435
TechAllByHarshit <41013322+TechAllByHarshit@users.noreply.github.com>
2020-11-21 20:55:17 +0530

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'kick',
     desccription: 'Kicks a user out of the server',
-    execute(message, args, bot) {
+    execute(message, args) {
         if(!message.member.permissions.has("KICK_MEMBERS")) {
             message.channel.send("You don't have the required permissions.");
             return;
@@ -15,7 +15,7 @@ module.exports = {
         message.channel.send("Its working i guess");
         const srole = message.member.roles.highest.position;
         const rrole = mentionMember.roles.highest.position;
-        const brole = bot.user.roles.highest.position;
+        const brole = message.guild.me.roles.highest.position;
         message.channel.send("Hello\n"+srole+"\n"+rrole+"\n"+brole);
     }
 }
