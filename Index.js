8181bc1b9d4c746f69838250c248d7cd6efc276c
TechAllByHarshit <41013322+TechAllByHarshit@users.noreply.github.com>
2020-11-27 13:18:46 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -25,7 +25,7 @@ bot.on("ready", ()=>{
 
 bot.on("message", message => {
     
-    if (message.author.bot) return;
+    if (message.author === bot) return;
     
     if (message.channel.type === ("dm")) {
         message.channel.send(`Hello! ${message.member.tag}.\nI do not accept commands in DM\nTo know my prefix in a server, just ping me and i will tell the prefix.`); 
