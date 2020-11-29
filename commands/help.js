f9afab757d487f699924c164a370b516f167b4b5
TechAllByHarshit <41013322+TechAllByHarshit@users.noreply.github.com>
2020-11-29 14:08:56 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,8 @@
 
+module.exports = {
+    name: 'ban',
+    desccription: 'kicks and bans a user out of the server',
+    execute(message, args) {
+        message.channel.send("Coming Soon.");
+    }
+}
