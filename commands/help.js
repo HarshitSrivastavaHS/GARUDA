89ee7cfdbcb77b7640808ed192012e1fb6ab0670
TechAllByHarshit <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-02 12:26:05 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,13 +2,15 @@
 module.exports = {
     name: 'help',
     description: 'shows the help menu',
-    execute(message, args, bot) {
+    execute(message, args, bot, Discord) {
         const fs = require('fs');
         message.channel.send("Coming Soon.");
         const commandFiles = fs.readdirSync(`./commands/`).filter(file => file.endsWith('.js'));
+        const helpembed = new Discord.MessageEmbed()
+        .setColor("#D441EE");
         for (const file of commandFiles) {  
             const command = require(`${__dirname}/${file}`).description;  
-            message.channel.send(`${file} --- ${command}`);
+            helpembed.addFiels(){name:`${file}`,value: `${command}`};
         }
     }
 }
