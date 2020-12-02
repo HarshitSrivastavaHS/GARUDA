7d3061c5e64f2619c60e38b200fd2b6d4e2ec72c
TechAllByHarshit <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-02 12:12:12 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
         message.channel.send("Coming Soon.");
         const commandFiles = fs.readdirSync(`./commands/`).filter(file => file.endsWith('.js'));
         for (const file of commandFiles) {  
-            const command = require(`${file}`).description;  
+            const command = require(`${__dirname}/${file}`).description;  
             message.channel.send(`${file} --- ${command}`);
         }
     }
