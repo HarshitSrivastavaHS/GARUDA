427db06bc8f00c6c7dcac12dd274d8d87b606985
TechAllByHarshit <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-03 11:44:30 +0530

    Update country.js

--- commands/country.js
+++ commands/country.js
@@ -2,8 +2,10 @@ module.exports = {
     name: 'country',
     description: 'IN DEVELOPMENET',
     execute(message, args, bot, fs) {
+        const code = args[0];
+        const country = fs.readFile(`https://restcountries.eu/rest/v2/alpha/${code}`)
         message.channel.send("Still in development");
         message.channel.send("Test messages below:::Please Ignore");
-        message.channel.send(args);
+        message.channel.send(JSON.parse(country));
     }
 }
