babd3c77b73aa50abe0d1e14bb2c746f9a14885b
TechAllByHarshit <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-03 11:20:49 +0530

    Update time.js

--- commands/time.js
+++ commands/time.js
@@ -2,6 +2,9 @@ module.exports = {
     name: 'time',
     description: 'IN DEVELOPMENET',
     execute(message, args, bot) {
+        const date = new Date().getTimezoneOffset();
         message.channel.send("Still in development");
+        message.channel.send("Test messages below:::Please Ignore");
+        message.channel.send(date);
     }
 }
