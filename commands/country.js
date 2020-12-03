4f28cd4f3416639711832deb62b5fc51e708aeea
TechAllByHarshit <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-03 12:25:36 +0530

    Update country.js

--- commands/country.js
+++ commands/country.js
@@ -4,9 +4,15 @@ module.exports = {
     execute(message, args, bot, fs) {
         const code = args[0];
         const fetch = require("node-fetch");
+        try {
             fetch(`https://restcountries.eu/rest/v2/alpha/${code}`).then((res)=>{
                 message.channel.send(JSON.parse(res.json));
             })
-        
+        }
+        catch (err){
+                message.channel.send(":broken-heart: Something went wrong");
+                message.channel.send("Ignore the next message");
+                message.channel.send(err);
+        }
     }
 }
