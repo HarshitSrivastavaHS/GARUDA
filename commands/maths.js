6efe58bd248d5dc28b67f585a046896f69c0b8bd
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-04 11:49:06 +0530

    Update maths.js

--- commands/maths.js
+++ commands/maths.js
@@ -3,9 +3,14 @@ module.exports = {
     description: 'does simple maths calculations',
     execute(message, args, bot, Discord) {
         if (args[0]==="add") {
-          if (!(args[1]==undefined)||!(args[2]==undefined)){
-            message.channel.send(`${args[1]}+${args[2]}=${parseFloat(args[1])+parseFloat(args[2])}`);
-          }
+            try {
+                const sum = parseFloat(args[1])+parseFloat(args[2]);
+            }
+            catch {
+                message.channel.send("Please enter numbers only.");
+                return;
+            }
+            message.channel.send(`${args[1]}+${args[2]}=${sum}`);
         }
         
         else if (args[0]==="sub") {
