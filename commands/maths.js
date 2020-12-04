d9f1f5fc926aa47012c28afc5ad1acbce3214f23
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-04 11:29:06 +0530

    Create maths.js

---
+++ commands/maths.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: 'maths',
+    description: 'does simple maths calculations',
+    execute(message, args, bot, Discord) {
+        if (args[0]==="add") {
+          if (!args[1]===undefined&&!args[2]===undefined){
+            message.channel.send(`${args[1]}+${args[2]}=${args[1]+args[2]}`);
+          }
+        }
+        
+        else if (args[0]==="sub") {
+          
+        }
+        
+        else if (args[0]==="mul") {
+          
+        }
+        
+        else if (args[0]==="div") {
+          
+        }
+        
+        else if (args[0]==="eval") {
+          
+        }
+        
+        else {
+          message.channel.send("Invalid syntax!");
+        }
+    }
+}
