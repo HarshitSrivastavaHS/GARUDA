c16a5702bd7b1aeb7c709ab011879cfe909b5cf5
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-04 12:10:05 +0530

    Update maths.js

--- commands/maths.js
+++ commands/maths.js
@@ -16,15 +16,42 @@ module.exports = {
         }
         
         else if (args[0]==="sub") {
-          
+            if (args[1]!=undefined&&args[2]!=undefined){
+                const diff = parseFloat(args[1])-parseFloat(args[2]);
+                if (!isNaN(diff))
+                    message.channel.send(`${args[1]}+${args[2]}=${diff}`);
+                else 
+                    message.channel.send("Please enter numbers only.")
+            }
+            else {
+                message.channel.send("Please enter numbers to be subtracted.")
+            }
         }
         
         else if (args[0]==="mul") {
-          
+            if (args[1]!=undefined&&args[2]!=undefined){
+                const pro = parseFloat(args[1])*parseFloat(args[2]);
+                if (!isNaN(pro))
+                    message.channel.send(`${args[1]}+${args[2]}=${pro}`);
+                else 
+                    message.channel.send("Please enter numbers only.")
+            }
+            else {
+                message.channel.send("Please enter numbers to be multiplied.")
+            }  
         }
         
         else if (args[0]==="div") {
-          
+            if (args[1]!=undefined&&args[2]!=undefined){
+                const quo = parseFloat(args[1])+parseFloat(args[2]);
+                if (!isNaN(quo) || quo==infinity)
+                    message.channel.send(`${args[1]}+${args[2]}=${quo}`);
+                else 
+                    message.channel.send("Please enter numbers only.")
+            }
+            else {
+                message.channel.send("Please enter numbers to be divided.")
+            }  
         }
         
         else if (args[0]==="eval") {
