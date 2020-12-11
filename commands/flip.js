d053b1ae116a1bd62da7a1a4744a2dc645c7d76e
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-11 20:19:18 +0530

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -2,6 +2,16 @@ module.exports = {
     name: 'flip',
     description: 'flips a coin',
     execute: async (message, args, bot, Discord) => {
-        
+        let faces = ['Head', 'Tail'];
+        let coin = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("Flip a coin");
+        let face = faces[Math.floor(Math.random(2))];
+        coin.setDescription(`The coin was flipped. It is ${face}.`)
+        switch(face) {
+            case 'Head': coin.setThumbnnail("https://www.pngitem.com/pimgs/m/123-1232373_coin-png-pic-heads-and-tails-indian-coin.png"); break;
+            case 'Tail': coin.setThumbnnail("https://thumbs.dreamstime.com/b/one-indian-rupee-coin-isolated-white-background-86022284.jpg"); break;    
+        }
+        message.channel.send(coin);
     }
 }
