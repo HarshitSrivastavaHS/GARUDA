3389af957d297ab113e9f2a9e0034fca192217da
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-11 17:42:31 +0530

    Create google.js

---
+++ commands/google.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'google',
+    description: 'searches the internet',
+    execute(message, args, bot, Discord) {
+        const request = require("node-superfetch");
+        let googleKey = process.env.GoogleAPI;
+        let csx = process.env.GoogleCSX;
+        
+        let query = args.join(" ");
+        if (!query) return message.channel.send("Please enter the query.");
+    }
+}
