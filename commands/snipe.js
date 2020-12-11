819cf9371614ff57cfb918ac04a01e9886aabe7d
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-11 21:46:28 +0530

    Create snipe.js

---
+++ commands/snipe.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'snipe',
+    description: 'shows last deleted message in that channel',
+    execute(message, args, bot, Discord) {
+        let msg = bot.snipes.get(message.channel.id);
+        if (!msg) return message.channel.send("There is no deleted message in this channel.");
+        let snipEMbed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(msg.author)
+        .setDescription(msg.content)
+        .setImags(msg.image?msg.image:null)
+    }
+}
