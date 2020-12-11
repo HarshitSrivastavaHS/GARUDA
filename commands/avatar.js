0ee81a959f97c097e0a98086f9c48ea1300c0e06
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-11 11:45:34 +0530

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'avatar',
+    description: 'shows the user's avatar',
+    execute(message, args, bot, Discord) {
+        const mentionUser = message.mentions.members.first();
+        const avataremb = new Discord.MessageEmbed()
+        .setColor("#D441EE")
+        .setTitle('Avatat')
+        .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL()}`)
+        .setFooter('Avatar Command');
+        if (mentionUser)
+          avataremb.setImage(`${mentionUser.displayAvatarURL()}`);
+        else
+          avataremb.setImage(`${message.author.displayAvatarURL()}`);
+        message.channel.send(avataremb);
+    }
+}
