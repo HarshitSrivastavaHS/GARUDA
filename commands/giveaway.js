92e95d96119ccccd1c492f6ce98623a1931c60bc
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-12 21:19:30 +0530

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -6,7 +6,7 @@ module.exports = {
       let time = args[0][1].toLowerCase;
       if (!time=="d"&&!time=="d"&&!time=="s") return message.channel.send("Invalid Syntax!\nFormat for time: 1d = 1day, 1m = 1 minute, 1s = 1 second\nExample:```%giveaway 1d Prize```");
       let timee = args[0].substr(0, str.length-1)
-      if (isNaN(timee) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
+      if (isNaN(timee)) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
       let prize = args.slice(1).join(" ");
       if (!prize) return message.channel.send("No prize specified. Please specify after the time.");
       let ms = 0;
