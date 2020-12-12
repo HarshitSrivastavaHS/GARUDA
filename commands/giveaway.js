e02e801b34816ae9d4748e733ac6b9899c67db58
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-12 21:18:34 +0530

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -3,9 +3,10 @@ module.exports = {
     description: 'to start a giveaway',
     execute: async (message, args, bot, Discord) => {
       if (!args[0]) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
-      let time = args[0].toLowerCase();
-      if (!time.endsWith("d")&&!time.endsWith("m")&&!time.endsWith("s")) return message.channel.send("Invalid Syntax!\nFormat for time: 1d = 1day, 1m = 1 minute, 1s = 1 second\nExample:```%giveaway 1d Prize```");
-      if (isNaN(time[0])) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
+      let time = args[0][1].toLowerCase;
+      if (!time=="d"&&!time=="d"&&!time=="s") return message.channel.send("Invalid Syntax!\nFormat for time: 1d = 1day, 1m = 1 minute, 1s = 1 second\nExample:```%giveaway 1d Prize```");
+      let timee = args[0].substr(0, str.length-1)
+      if (isNaN(timee) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
       let prize = args.slice(1).join(" ");
       if (!prize) return message.channel.send("No prize specified. Please specify after the time.");
       let ms = 0;
@@ -15,7 +16,7 @@ module.exports = {
       .setColor("PURPLE")
       .setDescription(`Hosted by ${message.author.tag} | Time ${time} | Prize ${prize}`)
       //.setTimestamp(Date.now() + ms);
-      let msg = await message.channel.send(giveawayEM);
+      let msg = await message.channel.send(":tada:GIVEAWAY:tada:"giveawayEM);
       msg.react("🎉");
     }
 }
