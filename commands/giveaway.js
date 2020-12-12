090fd79ef688921f2a85b15b4ed61f2f010ff071
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-12 20:43:35 +0530

    Create giveaway.js

---
+++ commands/giveaway.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'giveaway',
+    description: 'to start a giveaway',
+    execute: async (message, args, bot, Discord) => {
+      if (!args[0]) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
+      let time = args[0].toLowerCase();
+      if (!time.endsWith("d")&&!time.endsWith("m")&&!time.endsWith("s")) return message.channel.send("Invalid Syntax!\nFormat for time: 1d = 1day, 1m = 1 minute, 1s = 1 second\nExample:```%giveaway 1d Prize```");
+      if (isNaN(time[0])) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
+      let prize = args.slice(1).join(" ");
+      if (!prize) return message.channel.send("No prize specified. Please specify after the time.");
+      message.channel.send("Test Successful");
+    }
+}
