31f651848fab895bc53090f1ad6a6c8800e63457
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-12 17:27:03 +0530

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -1,27 +1,16 @@
 module.exports = {
     name: 'google',
     description: 'searches the internet',
-    execute:  (message, args, bot, Discord) => {
+    execute: async (message, args, bot, Discord) => {
         const request = require("node-superfetch");
         let googleKey = process.env.GoogleAPI;
         let csx = process.env.GoogleCSX;
         
         let query = args.join(" ");
         if (!query) return message.channel.send("Please enter the query.");
-        let href = "";
-        function search(query) {
-            const {body} = request.get("https://www.googleapis.com/customsearch/v1").query({
-                key: googleKey, cx: csx, safe: "off", q: query
-            });
-           
-            message.channel.send("Something went wrong.");
-            
-            if (!body.items) return;
-            
-            return body.items[0];
-        }
+        
         try {
-        href = search(query);
+        href = await search(query);
         }
         catch {
            message.channel.send("something went wrong.");
@@ -39,16 +28,20 @@ module.exports = {
         
         message.channel.send(searchemb);
         
-        function search(query) {
-            const {body} = request.get("https://www.googleapis.com/customsearch/v1").query({
+        async function search(query) {
+            const {body} = await request.get("https://www.googleapis.com/customsearch/v1").query({
                 key: googleKey, cx: csx, safe: "off", q: query
             });
            
             message.channel.send("Something went wrong.");
+            let hrefreturn = "";
+            try {
+                 hrefreturn = body.items[0];
+            }
+            catch{
             
-            if (!body.items) return;
-            
-            return body.items[0];
+            }
+            return hrefreturn;
         }
     }
 }
