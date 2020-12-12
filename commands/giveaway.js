a5df2449ef9d541f24b3efeb5f0e300c60c3e910
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-12 21:00:05 +0530

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'giveaway',
     description: 'to start a giveaway',
     execute: async (message, args, bot, Discord) => {
+      const ms = require("ms");
       if (!args[0]) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
       let time = args[0].toLowerCase();
       if (!time.endsWith("d")&&!time.endsWith("m")&&!time.endsWith("s")) return message.channel.send("Invalid Syntax!\nFormat for time: 1d = 1day, 1m = 1 minute, 1s = 1 second\nExample:```%giveaway 1d Prize```");
@@ -13,7 +14,7 @@ module.exports = {
       .setTitle("New Giveaway")
       .setColor("PURPLE")
       .setDescription(`Hosted by ${message.author.id} | Time ${time} | Prize ${prize}`)
-      .setTimestamp(Date.now + ms(time));
+      .setTimestamp(Date.now() + ms(time));
       let msg = message.channel.send(giveawayEM);
       msg.react(":tada:");
     }
