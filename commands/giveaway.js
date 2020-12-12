420f218dd7aef734f52c885e903f73d95e8d01f0
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-12 21:31:12 +0530

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -3,8 +3,9 @@ module.exports = {
     description: 'to start a giveaway',
     execute: async (message, args, bot, Discord) => {
       if (!args[0]) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
+      
+      if (!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")&&!args[0].endsWith("s")) return message.channel.send("Invalid Syntax!\nFormat for time: 1d = 1 day, 1h = 1 hour, 1m = 1 minute, 1s = 1 second\nExample:```%giveaway 1d Prize```");
       let time = args[0][1].toLowerCase;
-      if (!time=="d"&&!time=="d"&&!time=="s") return message.channel.send("Invalid Syntax!\nFormat for time: 1d = 1day, 1m = 1 minute, 1s = 1 second\nExample:```%giveaway 1d Prize```");
       let timee = args[0].substr(0, args[0].length-1)
       if (isNaN(timee)) return message.channel.send("Invalid Syntax!\nExample:```%giveaway 1d Prize```");
       let prize = args.slice(1).join(" ");
@@ -15,8 +16,8 @@ module.exports = {
       .setTitle(prize)
       .setColor("PURPLE")
       .setFooter("Ends at")
-      .setDescription(`React with :tada: to enter!\nTime ${time}\nHosted by ${message.author.tag}`)
-      //.setTimestamp(Date.now() + ms);
+      .setDescription(`React with :tada: to enter!\nTime ${timee}\nHosted by ${message.author.tag}`)
+      .setTimestamp(Date.now() + ms);
       message.channel.send(":tada:GIVEAWAY:tada:");
       let msg = await message.channel.send(giveawayEM);
       msg.react("🎉");
