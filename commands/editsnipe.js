6a937892829b62cff7561dcbcc1601e9d9ddccdb
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-12 16:52:03 +0530

    Create editsnipe.js

---
+++ commands/editsnipe.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'editsnipe',
+    description: 'shows last edited message in that channel',
+    execute(message, args, bot, Discord) {
+        let msg = bot.editSnipes.get(message.channel.id);
+        if (!msg) return message.channel.send("There is no edited message in this channel.");
+        let editSnipEMbed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setAuthor(msg.author, msg.avatar)
+        .addFields(
+        {name:Old Message,value: msg.oldContent},
+        {name:New Message,value: msg.newContent}
+        )
+        .setImage(msg.image?msg.image:null)
+        .setTimestamp();
+        message.channel.send(snipEMbed);
+    }
+}
