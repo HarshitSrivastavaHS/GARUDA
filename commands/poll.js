5fb501989375e0a71767f654c6bcd900674ba55e
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-13 14:51:49 +0530

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'poll',
+    description: 'starts a simple yes/no poll',
+    execute: async (message, args, bot, Discord) => {
+       if (!args) return message.channel.send("Invalid Syntax.\n```\n%poll <The yes/no question>\n```");
+       let question = args.join(" ");
+       let pollembed = new Discord.MessageEmbed()
+       .setColor("RANDOM")
+       .setAuthor("📃Poll📃")
+       .setTitle(question)
+       .setDescription("React with 👍 for yes. \nReact with 👎 for no.")
+       .setFooter(message.author)
+       .setTimestamp();
+       let msg = message.channel.send(pollembed)
+       msg.react("👍").then(()=>{msg.react("👎")})
+    }
+}
