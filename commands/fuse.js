3b5c016bd52522d6351dc01746f5da52fec16039
TechAllByHarshit <srivastavar025@gmail.com>
2020-12-18 20:21:48 +0530

    canvas test

--- commands/fuse.js
+++ commands/fuse.js
@@ -3,6 +3,12 @@ module.exports = {
     type: 'fun',
     description: 'fuses user\'s avatar with the mentioned user\'s avatar',
     execute(message, args, bot, Discord) {
-        
+        const Canvas = require("canvas");
+        const canvas = Canvas.createCanvas(800, 800);
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "test.png")
+        message.channel.send(attachment);
     }
 }
