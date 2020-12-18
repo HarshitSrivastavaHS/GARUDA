b41c6de83ba64c61233ded878bb025066e15f959
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-18 21:53:21 +0530

    Update fuse.js

--- commands/fuse.js
+++ commands/fuse.js
@@ -2,11 +2,12 @@ module.exports = {
     name: 'fuse',
     type: 'fun',
     description: 'fuses user\'s avatar with the mentioned user\'s avatar',
-    execute(message, args, bot, Discord) {
+    execute = async (message, args, bot, Discord) => {
         const Canvas = require("canvas");
         const canvas = Canvas.createCanvas(800, 800);
         const ctx = canvas.getContext("2d");
-        ctx.drawImage(message.author.displayAvatarURL(), 0, 0, canvas.width, canvas.height);
+        const img = await Canvas.loadImage(message.autho.displayAvatarURL);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "test.png")
         message.channel.send(attachment);
     }
