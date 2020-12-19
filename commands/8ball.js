a8f3135bd4a4111e4b3a88e682bcd235a0fd69ea
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-19 21:39:09 +0530

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,6 +3,6 @@ module.exports = {
     type: 'fun',
     description: 'ask yes/no question from the bot',
     execute: async (message, args, bot, Discord) => {
-        const replies = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again."];
+        const replies = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
     }
 }
