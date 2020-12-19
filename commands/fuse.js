3a543ee5d55c648c212be07622190af5bc448d04
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-19 10:46:30 +0530

    Update fuse.js

--- commands/fuse.js
+++ commands/fuse.js
@@ -7,6 +7,7 @@ module.exports = {
         const canvas = Canvas.createCanvas(800, 800);
         const mention = message.mentions.users.first();
         if (!mention) return message.channel.send("Please mention the user to fuse your avatar with.");
+        if (mention == message.author) return message.channel.send("You cannot fuse your Avatar with itself.");
         const ctx = canvas.getContext("2d");
         const img = await Canvas.loadImage(message.author.displayAvatarURL({format: "jpg", size: 4096}));
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
@@ -14,6 +15,6 @@ module.exports = {
         ctx.globalAlpha = 0.5;
         ctx.drawImage(img1, 0, 0, canvas.width,canvas.height);
         const attachment = new Discord.MessageAttachment(canvas.toBuffer(), `${message.author.username}${mention.username}.png`)
-        message.channel.send(`${message.author.username}'s and ${mention.username}'s Avatar's were fused together.`, attachment);
+        message.channel.send(`${message.author.username}'s and ${mention.username}'s Avatar were fused together.`, attachment);
     }
 }
