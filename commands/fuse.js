27262ecb7ae03cdec3d2eebc7d396e278d4ed955
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-19 10:32:04 +0530

    Update fuse.js

--- commands/fuse.js
+++ commands/fuse.js
@@ -5,9 +5,13 @@ module.exports = {
     execute: async (message, args, bot, Discord) => {
         const Canvas = require("canvas");
         const canvas = Canvas.createCanvas(800, 800);
+        const mention = message.mentions.users.first();
         const ctx = canvas.getContext("2d");
         const img = await Canvas.loadImage(message.author.displayAvatarURL({format: "jpg", size: 4096}));
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        const img1 = await Canvas.loadImage(mention.displayAvatarURL({format: "jpg", size: 4096}))
+        ctx.globalAlpha = 0.5;
+        ctx.drawImage(img1, 0, 0, canvas.width,canvas.height);
         const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "test.png")
         message.channel.send(attachment);
     }
