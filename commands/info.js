cb95829a6816f1462d9da5f83880f27a836e621c
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-20 13:22:12 +0530

    Update and rename details.js to info.js

--- commands/info.js
+++ commands/info.js
@@ -1,17 +1,18 @@
 module.exports = {
-    name: 'details',
+    name: 'info',
     description: 'Gives the details of the bot',
     execute(message, args, Discord, bot) {
         const detembed = new Discord.MessageEmbed()
         .setColor("#D441EE")
-        .setTitle("A-Bot")
-        .setAuthor("Harshit Srivastava")
+        .setTitle("TheTronBot")
+        .setThumbnail(message.author.displayAvatarURL())
         .addFields(
             { name: "Invite the bot", value: "https://discord.com/oauth2/authorize?client_id=777840690515279872&scope=bot&permissions=8" },
             { name: "Join the support Server" , value: "NA"}
         )
-        .setDescription("A-Bot is an advance yet simple bot made by TechAllByHarshit#3473 (<@451693463742840842>)")
-        .setImage(bot.user.displayAvatarURL());
-        message.channel.send(detembed);
+        .setDescription("TheTronBot is an multipurpose discord bot created by TechAllByHarshit#3473 (<@451693463742840842>)")
+        .setImage(bot.user.displayAvatarURL())
+        .setFooter(`Requested by ${message.author.username}`);
+        message.channel.send(":tada: TheTronBot :tada:",detembed);
     }
 }
