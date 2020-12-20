c52f7aaf4f0eb6a9d3552795cd952938dc9b4028
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-20 09:42:39 +0530

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,6 +6,10 @@ module.exports = {
         const replies = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
         if (args.length < 2) return message.channel.send("Please ask a full question");
         const opt = Math.floor(Math.random()*replies.length);
-        message.channel.send(`Q) ${args.join(" ")}\nA) ${replies[opt]}`);
+        let question = args.join(" ");
+        question = question[0].toUpperCase()+question.substr(1, question.length);
+        const embed = Discord.MessageEmbed()
+        .setTitle(question);
+        message.channel.send("Test Embed", embed);
     }
 }
