fb64d3766160125d591e74196c04e44f5151ad4b
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-20 13:30:10 +0530

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -7,12 +7,13 @@ module.exports = {
         .setTitle("TheTronBot")
         .setThumbnail(message.author.displayAvatarURL())
         .addFields(
-            { name: "Invite the bot", value: "https://discord.com/oauth2/authorize?client_id=777840690515279872&scope=bot&permissions=8" },
+            { name: "Invite the bot", value: "https://bit.ly/38j303G" },
             { name: "Join the support Server" , value: "NA"}
         )
+        .setTimestamp()
         .setDescription("TheTronBot is an multipurpose discord bot created by TechAllByHarshit#3473 (<@451693463742840842>)")
         .setImage(bot.user.displayAvatarURL())
         .setFooter(`Requested by ${message.author.username}`);
-        message.channel.send(":tada: TheTronBot :tada:",detembed);
+        message.channel.send(detembed);
     }
 }
