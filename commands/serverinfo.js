5f445e04ddb9211ab59ed7386d5bea7d1443c625
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-21 17:23:18 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,16 +2,16 @@ module.exports = {
     name: 'serverinfo',
     type: 'info',
     description: 'shows details of that server.',
-    execute(message, args, mybot, Discord) {
+    async execute(message, args, mybot, Discord) {
         let embed = new Discord.MessageEmbed()
         .setColor("YELLOW")
         .setAuthor(`${message.guild.name}`, message.guild.iconURL())
         .setThumbnail(message.guild.iconURL())
         .addFields(
-           {name: 'Owner', value: message.guild.owner.user.tag},
-           {name: 'Region', value: message.guild.region},
-           {name: 'Total Members', value: message.guild.memberCount},
-           {name: 'Total Bots', value: message.guild.members.cache.filter(m=>m.user.bot).size},
+           {name: 'Owner', value: await message.guild.owner.user.tag},
+           {name: 'Region', value: await message.guild.region},
+           {name: 'Total Members', value: await message.guild.memberCount},
+           {name: 'Total Bots', value: await message.guild.members.cache.filter(m=>m.user.bot).size},
            {name: 'Creation Date', value: message.guild.createdAt.toLocaleDateString({locales: "en-en"})},
         )
         message.channel.send(embed);
