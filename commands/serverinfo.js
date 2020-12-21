8c91d37ca6542a2d5a89f0fcf29070f540bd3510
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-21 16:55:30 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,6 +3,17 @@ module.exports = {
     type: 'info',
     description: 'shows details of that server.',
     execute(message, args, bot, Discord) {
-        
+        let embed = new Discord.MessageEmbed()
+        .setColor("YELLOW")
+        .setTitle(`${message.guild.name`)
+        .setThumbnail(message.guild.iconURL())
+        .addFields(
+           {name: 'Owner', value: message.guild.owner.user.tag},
+           {name: 'Region', value: message.guild.region},
+           {name: 'Total Members', value: message.guild.memberCount},
+           {name: 'Total Bots', value: message.guild.members.cache.filter(m=>m.user.bot).size},
+         
+        )
+        message.channel.send(embed);
     }
 }
