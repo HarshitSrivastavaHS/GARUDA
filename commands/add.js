814e61f885ea03a1be21ed1ecc18cbcdaace2457
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-21 11:04:13 +0530

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -4,15 +4,14 @@ module.exports = {
     type: 'maths',
     execute(message, args, bot, Discord) {
         if (args[1]!=undefined&&args[2]!=undefined){
-          const sum = parseFloat(args[1])+parseFloat(args[2]);
-          if (!isNaN(sum)) 
-            message.channel.send(`${args[1]}+${args[2]}=${sum}`);
-          else 
-            message.channel.send("Please enter numbers only.")
-          }
-          else {
+            const sum = parseFloat(args[1])+parseFloat(args[2]);
+            if (!isNaN(sum)) 
+                message.channel.send(`${args[1]}+${args[2]}=${sum}`);
+            else 
+                message.channel.send("Please enter numbers only.")
+            }
+        else {
             message.channel.send("Please enter numbers to be added.")
-          }
         }
     }
 }
