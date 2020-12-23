eeec27c69c8452e26c8c657164b09a8dee330162
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-23 11:12:26 +0530

    Update repeat.js

--- commands/repeat.js
+++ commands/repeat.js
@@ -2,12 +2,13 @@ module.exports = {
     name: 'repeat',
     description: 'repeats whatever the user wants it to repeat',
     execute(message, args, prefix) {
+        if (message.author != "451693463742840842") return message.channel.send("Fixing it. Only the owner can use it right now. Please keep patience :)");
         if (message.content.indexOf(' ') === -1) {
             message.reply("Invalid Syntax! ```\n%repeat <Text to be repeated by the bot>\n```");
             return;
         }
         message.delete();
-        console.log(message.everyone).catch(()=>console.log("an error occured"));
+        console.log(message.everyone);
         const reptext = message.content.substr(message.content.indexOf(' ')+1);
         message.channel.send(reptext);
     }
