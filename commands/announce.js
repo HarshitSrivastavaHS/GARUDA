9be83df1d024707430c2277cc4419ed7fac8688a
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-23 10:35:56 +0530

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -3,8 +3,8 @@ module.exports = {
     type: 'utility',
     usage: `%announce <#channel> <announcement>`,
     description: 'does an announcement',
-    execute(message, args, bot, Discord) {
+    async execute(message, args, bot, Discord) {
         if (args.length<=1) return message.channel.send("Invalid Syntax. Do `%help announce` for further details");
-        if (!args[0].channels) return message.channel.send("Hello World!");
+        if (!args[0].mentions.channels.first()) return message.channel.send("Hello World!");
     }
 }
