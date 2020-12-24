95b1533e7fcc40c1232e3a4ac7641749e68219f5
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-24 21:08:46 +0530

    Update welcome-schema.js

--- Schemas/welcome-schema.js
+++ Schemas/welcome-schema.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const reqString = {
   type: String,
-  require: true
+  required: true
 }
 
 const welcomeSchema = mongoose.Schema({
   _id: reqString,
   channelId: reqString,
-  message: reqString
+  message: reqString,
 })
 
 module.export = mongoose.model("welcome-channels", welcomeSchema)
