6da204a1a06fc80ba4c56095fcf5e4c2458d8df5
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-24 18:42:09 +0530

    Create welcome.js

---
+++ commands/welcome.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+module.exports = {
+    name: 'welcome',
+    type: 'utility',
+    description: 'sets the welcome channel',
+    async execute(message, args, bot, Discord) {
+        
+    }
+}
