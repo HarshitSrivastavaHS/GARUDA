4173ff4139bae7f60b6ded85ea0eedb2275002a2
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2020-12-24 19:12:32 +0530

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -7,9 +7,11 @@ module.exports = {
     async execute(message, args, bot, Discord) {
         if (!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send("Only an administrator can run this.")
         if (args.length<=1) return message.channel.send("Invalid syntax. Do `%help welcome` for more info.");
-        let Channel = args[0];
-        if (!Channel.mentions.channels) return message.channel.send("Please mention the channel.");
-        let channelid = Channel.mentions.channels;
+        
+        const CHANNELS_PATTERN = /<#(\d{17,19})>/g;
+        let Channel = CHANNELS_PATTERN.test(args[0]);
+        if (!Channel) return message.channel.send("Please mention the channel.");
+        let channelid = Channel.substr(2, Channel.length-2);
         message.channel.send(`GuildID: ${message.guild.id}\nChannelId: ${channelid}\nMessage: ${args.slice(1).join(" ")}`);
         
         
