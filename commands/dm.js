88bf104f69bc196ed62d8c84f89bcf9a7afb6315
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-01-03 10:20:04 +0530

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Kicks a user out of the server',
     async execute(message, args, bot, Discord, prefix) {
         if (args.length <= 1) return message.channel.send(`Invalid Syntax. ${prefix}help dm for more info.`)
-        const user = args[0].mentions.users.first();
+        const user = message.mentions.users.first();
         if (!user) return message.channel.send(`Invalid Syntax. ${prefix}help dm for more jnfo.`);
         const msg = args.slice(1).join(" ");
         if (!msg) return message.channel.send("Please enter the message to be sent.");
