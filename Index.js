e0671a6affcdddcb8b71e95e45ab7bc2b2107004
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-01-03 11:52:41 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -72,6 +72,12 @@ bot.prefixes = new Map();
 bot.suggestionChannel = new Map();
 bot.on("message", async (message) => {
 
+  if (message.author.bot) return;
+
+  if (message.channel.type === ("dm")) {
+    return;
+  }
+
   prefix = bot.prefixes.get(message.guild.id);
   if (!prefix) {
     await mongo().then(async (mongoose)=>{
@@ -92,13 +98,6 @@ bot.on("message", async (message) => {
         mongoose.connection.close()
       }
     })
-  }
-
-  if (message.author.bot) return;
-
-  if (message.channel.type === ("dm")) {
-    message.channel.send(`Hello ${message.author}!\nI do not accept commands in DM\nTo know my prefix in a server, just ping me and i will tell the prefix.`);
-    return;
   }
 
   if (message.mentions.members.has("777840690515279872")) {
