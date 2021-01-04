d2a2dd1c0bf0b7d0ab921d105147710557bbe206
Harshit Srivastava <srivastavar025@gmail.com>
2021-01-04 15:43:01 +0000

    dbblock command

--- Index.js
+++ Index.js
@@ -33,6 +33,9 @@ bot.on("ready", async () => {
     try {
       allDocuments = await giveawaySchema.find({})
     }
+    catch {
+      console.log("Error in giveaway continue")
+    }
     finally {
       mongoose.connection.close();
     }
@@ -68,6 +71,7 @@ bot.on("messageUpdate", (oldMessage, newMessage) => {
   });
 })
 
+bot.blocks = new Map();
 bot.prefixes = new Map();
 bot.suggestionChannel = new Map();
 bot.on("message", async (message) => {
@@ -93,6 +97,9 @@ bot.on("message", async (message) => {
           prefix = "%";
           bot.prefixes.set(message.guild.id, "%");
         }
+      }
+      catch {
+      console.log("Error in custom prefix")
       }
       finally {
         mongoose.connection.close()
@@ -207,6 +214,10 @@ bot.on("message", async (message) => {
  
   else if (command === 'dm') {
     bot.commands.get('dm').execute(message, args, bot, Discord, prefix);
+  }
+
+  else if (command === 'dmblock') {
+    bot.commands.get('dmblock').execute(message, args, bot, Discord, prefix);
   }
 
 })
