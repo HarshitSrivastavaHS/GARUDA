b39769f389f472b020fc6f6373c8b6e5976dfb43
Harshit Srivastava <srivastavar025@gmail.com>
2021-01-16 09:57:40 +0000

    Now **id** also works in avatar and trigger command

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,18 +4,12 @@ module.exports = {
     usage: `&{prefix}avatar <user's mention (optional)>`,
     description: 'shows the user\'s avatar',
     async execute(message, args, bot, Discord, prefix) {
-        const mentionUser = message.mentions.users.first();
+        const mentionUser = message.mentions.users.first() || bot.users.cache.get(args[0]) ||message.author;
         const avataremb = new Discord.MessageEmbed()
         .setColor("#D441EE")
         .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL({dynamic: true})}`);
-        if (mentionUser){
-          avataremb.setImage(`${mentionUser.displayAvatarURL({size: 4096, dynamic: true})}`)
-          .setTitle(`${mentionUser.tag}'s Avatar`);
-        }
-        else {
-          avataremb.setImage(`${message.author.displayAvatarURL({size: 4096, dynamic: true})}`)
-          .setTitle('Your Avatar');
-        }  
+        avataremb.setImage(`${mentionUser.displayAvatarURL({size: 4096, dynamic: true})}`)
+        .setTitle(`${mentionUser.tag}'s Avatar`);
         message.channel.send(avataremb);
     }
 }
--- commands/trigger.js
+++ commands/trigger.js
@@ -5,7 +5,7 @@ module.exports = {
     usage: '&{prefix}trigger <@mention>',
     description: 'triggers that person',
     async execute(message, args, bot, Discord, prefix) {
-      const user = message.mentions.users.first() || message.author;
+      const user = message.mentions.users.first() || bot.users.cache.get(args[0]) ||message.author;
       message.channel.startTyping();
       const img = await Canvacord.Canvas.trigger(user.displayAvatarURL({dynamic: false, format: "png"}));
       const attachment = new Discord.MessageAttachment(img, "triggered.gif");
