6d0a299d69738201ff6b803b1cf4185f3e9f2c2e
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-02-05 20:38:53 +0530

    Fuse works for two different people

--- commands/fuse.js
+++ commands/fuse.js
@@ -9,28 +9,70 @@ module.exports = {
 		const mention = message.mentions.users.first();
 		if (!mention)
 			return message.channel.send(
-				'Please mention the user to fuse your avatar with.'
+				'Please mention the user to fuse your avatar with or mention two user\'s to fuse their avatars.'
 			);
-		if (mention == message.author)
-			return message.channel.send('You cannot fuse your Avatar with itself.');
-		const ctx = canvas.getContext('2d');
-		const img = await Canvas.loadImage(
-			message.author.displayAvatarURL({ format: 'jpg', size: 4096 })
-		);
-		ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-		const img1 = await Canvas.loadImage(
-			mention.displayAvatarURL({ format: 'jpg', size: 4096 })
-		);
-		ctx.globalAlpha = 0.5;
-		ctx.drawImage(img1, 0, 0, canvas.width, canvas.height);
-		const attachment = new Discord.MessageAttachment(
-			canvas.toBuffer(),
-			`${message.author.username}${mention.username}.png`
-		);
 
-		message.channel.send(
-			`${message.author}'s and ${mention}'s Avatar were fused together.`,
-			attachment
-		);
+    		let string;
+    		const ctx = canvas.getContext('2d');
+    		let attachment;
+    		let address;
+    		message.channel.startTyping();
+   		 let user2 = message.mentions.users.array().length>1?message.mentions.users.array()[1]:null;
+    
+ 		   if (mention == user2) {
+		      user2 = null;
+		    }
+	
+		    if (!user2) {
+ 		     if (mention == message.author){
+  		      message.channel.stopTyping();
+			  return message.channel.send('You cannot fuse your Avatar with itself.');
+   		   }
+      
+		  const img = await Canvas.loadImage(
+			  message.author.displayAvatarURL({ format: 'jpg', size: 4096 })
+		  );
+		  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+		  const img1 = await Canvas.loadImage(
+			mention.displayAvatarURL({ format: 'jpg', size: 4096 })
+		  );
+		  ctx.globalAlpha = 0.5;
+		  ctx.drawImage(img1, 0, 0, canvas.width, canvas.height);
+		  attachment = new Discord.MessageAttachment(
+		  	canvas.toBuffer(),
+		  	`${message.author.username}${mention.username}.png`
+		  );
+
+   		   address = `${message.author.username}${mention.username}.png`;
+
+   		   string = `${message.author.username}'s and ${mention.username}'s Avatar were fused together.`;
+      
+   		 }
+
+   		 else {
+		  const img = await Canvas.loadImage(
+			  user2.displayAvatarURL({ format: 'jpg', size: 4096 })
+		  );
+		  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+		  const img1 = await Canvas.loadImage(
+			mention.displayAvatarURL({ format: 'jpg', size: 4096 })
+		  );
+		  ctx.globalAlpha = 0.5;
+		  ctx.drawImage(img1, 0, 0, canvas.width, canvas.height);
+		  attachment = new Discord.MessageAttachment(
+		  	canvas.toBuffer(),
+		  	`${mention.username}${user2.username}.png`
+		  );
+    		  address = `${mention.username}${user2.username}.png`;
+  		    string = `${mention.username}'s and ${user2.username}'s Avatar were fused together.`
+      
+ 		   }
+ 		   const embed = new Discord.MessageEmbed()
+  		    .setTitle(string)
+  		    .setColor("YELLOW")
+   		   .attachFiles(attachment)
+   		   .setImage(`attachment://${address}`);
+   		   message.channel.send(embed);
+			message.channel.stopTyping();
 	}
 };
