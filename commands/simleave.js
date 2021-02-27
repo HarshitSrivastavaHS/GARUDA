352f7e452d4d9371573899b7d5dd00561d04f4db
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-02-27 15:17:11 +0530

    Update simleave.js

--- commands/simleave.js
+++ commands/simleave.js
@@ -5,7 +5,7 @@ module.exports = {
     usage: "&{prefix}simleave", 
     async execute(message, args, bot, Discord, prefix) {
         if (!message.member.permissions.has("ADMINISTRATION")) return message.reply("Only an administrator can use this.");
-        let wc = bot.serverConfig.get(message.guild.id)?bot.serverConfig.get(message.guild.id).leave:null;
+        let wc = bot.serverConfig.get(message.guild.id)!=undefined?bot.serverConfig.get(message.guild.id).leave:undefined;
         if (!wc) return message.channel.send("NO leave channel set");
         bot.emit("guildMemberRemove", message.member);
     }
