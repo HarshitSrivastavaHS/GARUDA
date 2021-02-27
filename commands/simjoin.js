b610869f2ef4b305cdc5d7d00dc7f6c68dd06aec
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-02-27 15:04:45 +0530

    Update simjoin.js

--- commands/simjoin.js
+++ commands/simjoin.js
@@ -5,7 +5,7 @@ module.exports = {
     usage: "&{prefix}simjoin", 
     async execute(message, args, bot, Discord, prefix) {
         if (!message.member.permissions.has("ADMINISTRATION")) return message.reply("Only an administrator can use this.");
-        let wc = bot.serverConfig.get(message.guild.id)?bot.serverConfig.get(message.channel.id).welcome:null;
+        let wc = bot.serverConfig.get(message.guild.id)?bot.serverConfig.get(message.guild.id).welcome:null;
         if (!wc) return message.channel.send("NO welcome channel set");
         bot.emit("guildMemberAdd", message.member);
     }
