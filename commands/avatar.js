333bdea1b7c43feadda73b6b77324acfb117eaf3
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-05 20:53:44 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,7 +4,7 @@ module.exports = {
     usage: `&{prefix}avatar <user's mention (optional)>`,
     description: 'shows the user\'s avatar',
     async execute(message, args, bot, Discord, prefix) {
-        const mentionUser = message.mentions.users.first() || bot.users.cache.get(args[0]) || bot.users.find(user => user.username == args.join(" ")) ||message.author;
+        const mentionUser = message.mentions.users.first() || bot.users.cache.get(args[0]) || bot.users.cache.find(user => user.username == args.join(" ")) ||message.author;
         const avataremb = new Discord.MessageEmbed()
         .setColor("#D441EE")
         .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL({dynamic: true})}`);
