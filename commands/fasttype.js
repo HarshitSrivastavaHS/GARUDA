2969c62a70dab9fe63540179f1fcbbba135986f9
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:03:11 +0530

    Update fasttype.js

--- commands/fasttype.js
+++ commands/fasttype.js
@@ -63,7 +63,17 @@ module.exports = {
     type: 'game',
     usage: '&{prefix}fasttype',
     description: 'starts a typing game!',
+    permissions: ['SEND_MESSAGES', 'MANAGE_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
       
     const { channel } = message;
     message.delete();
