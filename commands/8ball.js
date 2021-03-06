5d0d3c5d636cc1b2853aca9843af069423605439
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 16:47:02 +0530

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -4,6 +4,7 @@ module.exports = {
     usage: `&{prefix}8ball <question>`,
     description: 'ask yes/no question from the bot',
     async execute(message, args, bot, Discord, prefix) {
+        if (!(message.channel.permissionsFor(bot.user).has("EMBED_LINKS"))) return message.channel.send("The Following permissions are missing:\nEMBED_LINKS").catch(err=>console.log("Missing Send Message."));
         const replies = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
         if (args.length < 2) return message.channel.send("Please ask a full question");
         const opt = Math.floor(Math.random()*replies.length);
