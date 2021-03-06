e4c669d93b62b23d16686b55c682ea885d269e40
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 17:41:51 +0530

    Update banner.js

--- commands/banner.js
+++ commands/banner.js
@@ -4,7 +4,17 @@ module.exports = {
     type: 'fun',
     usage: '&{prefix}banner <text>',
     description: 'types that text in banner form',
+    permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
       if (args.length==0) return message.channel.send("Please enter the text.");
       const banner = await figlet(args.join("\n"));
       if (banner.length>2000) {
