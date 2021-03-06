7ef624a03a46e3acd3c25aafd1e634bd0a0acde9
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 17:34:46 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,17 @@ module.exports = {
     type: 'fun',
     usage: `&{prefix}avatar <user's mention (optional)>`,
     description: 'shows the user\'s avatar',
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         const mentionUser = message.mentions.users.first() || bot.users.cache.get(args[0]) || bot.users.cache.find(user => user.username == args.join(" ")) ||message.author;
         const avataremb = new Discord.MessageEmbed()
         .setColor("#D441EE")
