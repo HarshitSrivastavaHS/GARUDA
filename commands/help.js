302fe200e639dad413ada370018ab5d2c3796581
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:07:25 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,17 @@
 module.exports = {
     name: 'help',
     description: 'shows the help menu',
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         const PREFIX_REG = /&{prefix}/g;
         const fs = require('fs');
         const commandFiles = fs.readdirSync(`./commands/`).filter(file => file.endsWith('.js'));
