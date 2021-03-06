6a7996c77d73a52337a15bd57b5e134277081143
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 17:14:41 +0530

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,7 +5,11 @@ module.exports = {
     description: 'ask yes/no question from the bot',
     permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
-        let botPerms = this.permissions.forEach(p=>{message.channel.permissionsFor(bot.user).has(p)});
+        
+        let botPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+        })
         console.log(botPerms);
         if (!(message.channel.permissionsFor(bot.user).has("EMBED_LINKS"))) return message.channel.send(`The Following permissions are needed by the bot for this command:\nEMBED_LINKS`).catch(err=>console.log(`Missing send message permission in a server.`));
         const replies = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
