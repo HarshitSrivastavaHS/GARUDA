5cca443623cd4d821eb45233e38d8f1da92f5eda
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:04:02 +0530

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -3,7 +3,17 @@ module.exports = {
     type: 'fun',
     usage: '&{prefix}flip',
     description: 'flips a coin',
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         let faces = ['Head', 'Tail'];
         let coin = new Discord.MessageEmbed()
         .setColor("RANDOM")
