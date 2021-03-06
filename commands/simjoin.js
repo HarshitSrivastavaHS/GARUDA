efca8dbc0d6c33a242e01eadfb9e7867fa0de365
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:33:19 +0530

    Update simjoin.js

--- commands/simjoin.js
+++ commands/simjoin.js
@@ -3,7 +3,17 @@ module.exports = {
     type: 'admin',
     description: 'simulates join for testing',
     usage: "&{prefix}simjoin", 
+    permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         if (!message.member.permissions.has("ADMINISTRATION")) return message.reply("Only an administrator can use this.");
         let wc = bot.serverConfig.get(message.guild.id)!=undefined?bot.serverConfig.get(message.guild.id).welcome:undefined;
         if (!wc) return message.channel.send("NO welcome channel set");
