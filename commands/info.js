07d64ce863ae8b995df30b59b85d31d6f5bed27f
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:06:51 +0530

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,17 @@
 module.exports = {
     name: 'info',
     description: 'Gives the details of the bot',
+   permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         const detembed = new Discord.MessageEmbed()
         .setColor("#D441EE")
         .setTitle("GARUDA")
