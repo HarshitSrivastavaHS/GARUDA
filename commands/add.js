cc6ad0f9aecf2fc12472f9ac5b0e923d73573bae
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 17:30:55 +0530

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -3,7 +3,17 @@ module.exports = {
     description: 'adds numbers',
     type: 'maths',
     usage: '&{prefix}add <num1> <num2> <num3> ... <num-n>',
+    permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         if (args.length <= 1) return message.channel.send("Please enter numbers to be added.");
         let sum = 0;
         let lhs = "";
