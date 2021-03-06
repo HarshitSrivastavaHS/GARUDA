cdb0a19be976385b8c413f765303d79362250f60
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:15:20 +0530

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,11 +1,21 @@
 const fetch = require('node-fetch');
 const { Util, MessageEmbed } = require ('discord.js');
 module.exports = {
-  name: 'lyrics', 
-  type: 'utility',
-  usage: '&{prefix}lyrics <Song name>', 
-  description:'Retrieves the lyrics of the given song name',
-  async execute(message,args, bot, Discord,prefix) {
+    name: 'lyrics', 
+    type: 'utility',
+    usage: '&{prefix}lyrics <Song name>', 
+    description:'Retrieves the lyrics of the given song name',
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
+    async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
     if (args.length<1) return message.channel.send("**Please enter the name of the song.**")
     message.channel.startTyping();
     const song = await fetch(`https://some-random-api.ml/lyrics?title=${args.join(" ")}`).then(r=> r.json());
