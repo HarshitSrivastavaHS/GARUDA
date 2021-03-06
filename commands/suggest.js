418a8424066d0dafb8cf7cdb3ae4caf343b7fbc7
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:40:54 +0530

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'suggest',
     description: 'send suggestions in the suggestion channel.',
     usage: '&{prefix}suggest <suggestion>',
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS', 'ADD_REACTIONS'],
     async execute(message, args, bot, Discord, prefix) {
         if (args<1) return message.channel.send(`Please type the suggestion also.`)
         let mes = await message.channel.send("Searching for the Suggestion Channel.");
@@ -9,6 +10,16 @@ module.exports = {
         if (!suggestionchannel) return mes.edit(`No suggestion channel set. If you are an administrator, please do \`${prefix}help setsuggestion\` to know how to set a suggestion channel.`)
       
         let channel = message.guild.channels.cache.get(suggestionchannel);
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
+        
         const embed = new Discord.MessageEmbed()
         .setColor("YELLOW")
         .setTitle("Suggestion")
@@ -27,7 +38,7 @@ module.exports = {
         mes.edit(`Suggestion posted in <#${suggestionchannel}>`).then((msg)=>{
           setTimeout(()=>{
             msg.delete()
-            message.delete()
+            message.delete().catch();
           },3000)
         })
     }
