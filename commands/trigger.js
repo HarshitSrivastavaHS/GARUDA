1091cbef6ac21fa7924158274d6f6521cc526284
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:47:06 +0530

    Update trigger.js

--- commands/trigger.js
+++ commands/trigger.js
@@ -4,7 +4,18 @@ module.exports = {
     type: 'fun',
     usage: '&{prefix}trigger <@mention>',
     description: 'triggers that person',
+    permissions: ['SEND_MESSAGES', 'ATTACH_FILES'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
+        
       const user = message.mentions.users.first() || bot.users.cache.get(args[0]) ||message.author;
       message.channel.startTyping();
       const img = await Canvacord.Canvas.trigger(user.displayAvatarURL({dynamic: false, format: "png"}));
