289250be706fabfbb762b8d0196dfda741e9beb0
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 17:49:59 +0530

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -3,7 +3,17 @@ module.exports = {
     type: 'fun',
     usage: '&{prefix}dog',
     description: 'get a random dog pic',
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         message.channel.startTyping();
         const fetch = require("node-fetch");
         try {
