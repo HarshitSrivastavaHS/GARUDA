181a3cb69b091cdacea8997574faa01dfbf1ab19
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:47:28 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -2,7 +2,18 @@ module.exports = {
     name: 'vote',
     usage: '&{prefix}vote',
     description: 'support the bot by voting for the bot.',
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
+        
        const emb = new Discord.MessageEmbed()
         .setColor("PURPLE")
         .setTitle("Vote for GARUDA")
