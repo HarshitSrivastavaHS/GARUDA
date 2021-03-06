765c7aee20c84c8adb072d1d961012078eec39c9
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 18:16:53 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,17 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'ping',
     description: 'This is the ping command',
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
       const cnt = new Date().getTime();
       const ping = cnt - message.createdAt;
      var icon = message.guild.iconURL() 
