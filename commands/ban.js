2583000e25e6ec43163499e4bbd6b13f33a723ad
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 17:37:53 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,7 +3,17 @@ module.exports = {
     type: 'moderation',
     usage: '&{prefix}ban <user\'s @>',
     description: 'kicks and bans a user out of the server',
+    permissions: ['SEND_MESSAGES', 'BAN_MEMBERS'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         if(!message.member.permissions.has("BAN_MEMBERS")) {
             message.channel.send("You don't have the required permissions.");
             return;
