dca10b40c42f49f5de1ad652ff4a9b33811798ca
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 17:32:43 +0530

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -3,11 +3,21 @@ module.exports = {
     name: 'afk',
     usage: '&{prefix}afk',
     description: 'adds/removes [AFK] in your nickname.',
+    permissions: ['SEND_MESSAGES', 'MANAGE_NICKNAMES'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
       const srole = message.member.roles.highest.position;
       const brole = message.guild.me.roles.highest.position;
-      if (srole>=brole || !message.guild.me.permissions.has("MANAGE_NICKNAMES")) {
-      return message.channel.send("I do not have the required permissions.")
+      if (srole>=brole) {
+      return message.channel.send("Your role is at a higher postition than mine.")
       }
       let nick = message.member.nickname;
       if (nick==null) {
