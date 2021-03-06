8e2387735028f9abde6d2cbf555fe4133a48d586
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-06 17:55:00 +0530

    Update editsnipe.js

--- commands/editsnipe.js
+++ commands/editsnipe.js
@@ -3,7 +3,17 @@ module.exports = {
     type: 'utility',
     usage: '&{prefix}editsnipe',
     description: 'shows last edited message in that channel',
+    permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         let msg = bot.editSnipes.get(message.channel.id);
         if (!msg) return message.channel.send("There is no edited message in this channel.");
         let editSnipEMbed = new Discord.MessageEmbed()
