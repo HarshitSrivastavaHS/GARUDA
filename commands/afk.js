beddfd126b0a77d78e19e780aa2f2fd0537c1995
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-26 21:46:37 +0530

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,9 +1,9 @@
-const randomPuppy = require("random-puppy");
+let bot.afk = new Map();
 module.exports = {
     name: 'afk',
     usage: '&{prefix}afk',
-    description: 'adds/removes [AFK] in your nickname.',
-    permissions: ['SEND_MESSAGES', 'MANAGE_NICKNAMES'],
+    description: 'sets AFK.',
+    permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
         let botPerms = [];
         let missingPerms = [];
@@ -14,25 +14,8 @@ module.exports = {
         })
         missingPerms = missingPerms.join("\n");
         if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
-      const srole = message.member.roles.highest.position;
-      const brole = message.guild.me.roles.highest.position;
-      if (srole>=brole) {
-      return message.channel.send("Your role is at a higher postition than mine.")
-      }
-      let nick = message.member.nickname;
-      if (nick==null) {
-        nick = message.author.username;
-      }
-      if (nick.startsWith("[AFK]")){
-        nick = nick.replace(/\[AFK\]/,"");
-        message.member.setNickname(nick);
-        return;
-      }
-      else {
-        nick = `[AFK] ${nick}`;
-        if (nick.length>32)
-          nick = nick.substr(0,32);
-        message.member.setNickname(nick);
-      }
+        let afkmsg = args.length<1?"AFK":args.join(" ");
+        bot.afk.set(message.author.id, afkmsg);
+        message.reply(`AFK successfully set, msg: ${afkmsg}`);
     }
 }
