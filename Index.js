d3d2d9a1853ae77f70c5c820d13f718867e826a3
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-27 19:56:31 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -125,7 +125,8 @@ bot.on('messageDelete', (message, channel) => {
 				.setFooter("Bot By TechAllByHarshit")
 				.setDescription(`Message:\n\n${message.content}`)
 				.addField("Channel", message.channel)
-				.addFields("Message Author", message.author)
+				.addFields("Message Author", message.author);
+				tarch.send(ghostEM);
 			}
 		}
 	}
