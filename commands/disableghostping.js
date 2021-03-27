b74b396f65ca6f0f41d56e69b87c18cb1728d14d
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-27 19:27:07 +0530

    Create disableghostping.js

---
+++ commands/disableghostping.js
@@ -0,0 +1,43 @@
+const mongo = require(`../mongo`);
+const serverConfig = require('../Schemas/server-config');
+module.exports = {
+	name: 'disableghostping',
+	type: 'admin',
+	usage: "&{prefix}disableghostping",
+	description: "Disables the ghost ping",
+	permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
+    async execute(message, args, bot, Discord, prefix) {
+        let botPerms = [];
+        let missingPerms = [];
+        this.permissions.forEach(p=>{
+            botPerms.push(message.channel.permissionsFor(bot.user).has(p));
+            if (!(message.channel.permissionsFor(bot.user).has(p)))
+                missingPerms.push(p);
+        })8JDhj2Cn3RVzArn
+        missingPerms = missingPerms.join("\n");
+        if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
+        let msg = await message.channel.send("Disabling the ghost ping channel.");
+        await mongo().then(async (mongoose)=>{
+          
+            await serverConfig.findOneAndUpdate({
+                    _id: message.guild.id
+                },{
+                    _id: message.guild.id,
+                    ghost: undefined,
+                },{
+                    upsert: true
+                })
+          
+          msg.edit(`Successfully disabled the ghost ping channel/command/well idk.`);
+          let result = bot.serverConfig.get(message.guild.id);
+          bot.serverConfig.set(message.guild.id, {
+            prefix: result.prefix,
+            suggestion: result.suggestion,
+            welcome: result.welcome,
+            leave: result.leave,
+            modLog: result.modLog,
+            ghost: undefined
+          });
+        })
+	}
+};
