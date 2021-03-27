fc7e250e61ccc2357494f87569b1be7f03f0aa42
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-03-27 16:38:14 +0530

    Create afk.js

---
+++ Schemas/afk.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const reqString = {
+  type: String,
+  required: true
+}
+
+const string = {
+  type: String,
+  required: false
+}
+
+const afk = mongoose.Schema({
+  _id: reqString,
+  afk: string
+})
+
+module.exports = mongoose.model("AFK", afk)
