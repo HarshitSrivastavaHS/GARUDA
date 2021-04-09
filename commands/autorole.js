5e7af4a07b34aa5d87c1f1dc240b3ff3228ee372
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-04-09 19:09:47 +0530

    Update autorole.js

--- commands/autorole.js
+++ commands/autorole.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(message, args, bot, Discord, prefix) {
       if (!message.member.permissions.has("MANAGE_SERVER"))
         return message.reply("You don't have the required permissions.");
-      if (!(args[0]||args[0].toLowerCase()=="add"||args[0].toLowerCase()=="remove")&&!args[1]) return message.reply(`Invalid Syntax. The following things are required: \nAdd or Remove\nRole mention or id\nUsage: ${prefix}autorole add @members`)
+      if (!(args[0]||args[0].toLowerCase()=="add"||args[0].toLowerCase()=="remove"||args[1])) return message.reply(`Invalid Syntax. The following things are required: \nAdd or Remove\nRole mention or id\nUsage: ${prefix}autorole add @members`)
       let subcmd = args[0];
       let role = message.guild.roles.cache.get(args[1]);
       if (!role) return message.reply("Invalid role id or mention.");
