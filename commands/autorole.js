2b46c42b55d1bb4ede3b04e4be9801c11d6686dd
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-04-09 19:44:16 +0530

    Update autorole.js

--- commands/autorole.js
+++ commands/autorole.js
@@ -7,10 +7,46 @@ module.exports = {
     async execute(message, args, bot, Discord, prefix) {
       if (!message.member.permissions.has("MANAGE_SERVER"))
         return message.reply("You don't have the required permissions.");
-      if (!args[0]||(args[0].toLowerCase()!="add"&&args[0].toLowerCase()!="remove")||!args[1]) return message.reply(`Invalid Syntax. The following things are required: \nAdd or Remove\nRole mention or id\nUsage: ${prefix}autorole add @members`)
+      if (!args[0]||(args[0].toLowerCase()!="add"&&args[0].toLowerCase()!="remove")) return message.reply(`Invalid Syntax. ${prefix}autorole <add/remove> <role id or mention>\nRole is required only if you are setting auto role.`)
       let subcmd = args[0];
-      let role = message.guild.roles.cache.get(args[1]);
-      if (!role) return message.reply("Invalid role id or mention.");
-      message.channel.send(role.id);
+        if (subcmd.toLowerCase()=="add") {
+            if (!args[1]) return message.reply(`Invalid Synatx. ${prefix}autorole add <role id or mention>`);
+        let role = message.guild.roles.cache.get(args[1]);
+        if (!role) return message.reply("Invalid role id or mention.");
+        let msg = await message.channel.send("Setting autoRole");
+         await serverConfig.findOneAndUpdate({
+                    _id: message.guild.id
+                },{
+                    _id: message.guild.id,
+                    autoRole: role.id,
+                },{
+                    upsert: true
+                })
+          msg.edit(`AutoRole set successfully. New users would be given the role upon their joining. (Bots won't get the role)`)
+          let result = bot.serverConfig.get(message.guild.id);
+          bot.serverConfig.set(message.guild.id, {
+            prefix: result.prefix,
+            suggestion: result.suggestion,
+            welcome: result.welcome,
+            leave: result.leave,
+            modLog: result.modLog,
+            ghost: result.ghost,
+            autoRole: role.id
+          });
+            
+      }
+      else {
+        let autoR = bot.serverConfig.get(member.guild.id)!=undefined?bot.serverConfig.get(member.guild.id).autoRole:undefined;
+        if (!autoR) message.channel.send("This server isn't using the autoRole feature of the bot.");
+        bot.serverConfig.set(message.guild.id, {
+            prefix: result.prefix,
+            suggestion: result.suggestion,
+            welcome: result.welcome,
+            leave: result.leave,
+            modLog: result.modLog,
+            ghost: result.ghost,
+            autoRole: undefined
+          });
+      }
     }
 }
