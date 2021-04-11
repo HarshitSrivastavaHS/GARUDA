e73f3d5337a50add17adbcfcc169a7bef7759ceb
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-04-11 08:57:21 +0530

    Update welcome.js

--- util/welcome.js
+++ util/welcome.js
@@ -26,7 +26,7 @@ module.exports = {
 	    ctx.fillStyle = '#edf2f4';
 	    ctx.fillText(text, 250, canvas.height / 1.8);
       ctx.font = `bold 28px sans-serif`;
-      ctx.fillText(`Humans: ${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`, 250+ctx.measureText(`Member #${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`).width/2, canvas.height / 3.5 + canvas.height / 1.8 - 10);
+      ctx.fillText(`Humans: ${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`, 250+225-ctx.measureText(`Member #${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`).width/2, canvas.height / 3.5 + canvas.height / 1.8 - 10);
       ctx.beginPath();
 	    ctx.arc(125, 125, 75, 0, Math.PI * 2, true);
     	ctx.closePath();
