f5da8ca5f8efe583df55fbfa4a3a995693b9b64c
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-04-21 18:38:17 +0530

    Update fasttype.js

--- commands/fasttype.js
+++ commands/fasttype.js
@@ -22,7 +22,7 @@ module.exports = {
         
         if (args[0]) {
             if (isNaN(args[0])) return message.channel.send("Please specify number of words only. This is optional. Default: 10")
-            if (args[0]<3&&args[0]>25) 
+            if (Number(args[0])<3&&Number(args[0])>25) 
                 maxWords = args[0];
         }
         
