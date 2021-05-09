c13ca086265ae45ced97829b953ae4acb47d35f2
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-05-09 17:46:28 +0530

    Update giveaway.js

--- functions/giveaway.js
+++ functions/giveaway.js
@@ -1,10 +1,10 @@
 
-module.exports = async (bot, Discord, msg, time, prize, ch) => {
+module.exports = async (bot, Discord, msg, time, winners, prize, ch) => {
   const mongo = require("../mongo.js")
   const giveawaySchema = require("../Schemas/giveaway-schema.js")
   let ms = time-Date.now();
   if (ms<0) {
-    ms = 10;
+    ms = 10000;
   }
   setTimeout(async ()=>{
     await mongo().then(async (mongoose)=>{
@@ -16,22 +16,30 @@ module.exports = async (bot, Discord, msg, time, prize, ch) => {
     if (!giveawayChannel) return;   
     msg = await giveawayChannel.messages.fetch(msg);
     if (!msg) return;
-    if (msg.reactions.cache.get("🎉").count <= 1) {
+    if (msg.reactions.cache.get("🎉").count <= winners) {
         let nowin = new Discord.MessageEmbed()
         .setColor("RED")
         .setTitle(prize)
-        .setDescription("No Winner")
+        .setDescription("Could not determine winner(s).")
         .setFooter(`Ended at`)
         .setTimestamp();
         msg.edit("**🎉Giveaway Ended🎉**", nowin);
-        return giveawayChannel.send("No one participated in the giveaway.");
+        return giveawayChannel.send(`Could not determine a winner.\nhttps://discord.com/channels/${giveawayChannel.guild.id}/${giveawayChannel.id}/${msg}`);
     }
     await msg.reactions.cache.get("🎉").users.fetch()
-    let winner = msg.reactions.cache.get("🎉").users.cache.filter((b)=>!b.bot).random();
+    let winner = "";
+    if (winners>1) {
+      msg.reactions.cache.get("🎉").users.cache.filter((b)=>!b.bot).random(winners)forEach((item, index)=>{
+        winner += item+"\n"
+      });
+    }
+    else {
+      winner = msg.reactions.cache.get("🎉").users.cache.filter((b)=>!b.bot).random();
+    }
     let winem = new Discord.MessageEmbed()
     .setColor("GREEN")
     .setTitle(prize)
-    .setDescription (`Winner\n${winner}`)
+    .setDescription (`${winners>1?"Winners:":"Winner"}\n${winner}`)
     .setFooter("Ended at")
     .setTimestamp()
     msg.edit("**🎉Giveaway Ended🎉**", winem);
@@ -41,6 +49,6 @@ module.exports = async (bot, Discord, msg, time, prize, ch) => {
     .setDescription(`Congratulations! You have won the giveaway for [${prize}](https://discord.com/channels/${giveawayChannel.guild.id}/${giveawayChannel.id}/${msg.id}) in ${giveawayChannel.guild.name}`)
     .setFooter(`${giveawayChannel.guild.name} - #${giveawayChannel.name}`);
     bot.users.cache.get(winner.id).send(winDM);
-    giveawayChannel.send(`${winner} has won the giveaway for **${prize}**.`)
+    giveawayChannel.send(`${winner} has won the giveaway for **${prize}**.\nhttps://discord.com/channels/${giveawayChannel.guild.id}/${giveawayChannel.id}/${msg}`)
    }, ms)
 }
