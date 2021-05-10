788f39c0f7d9346d388583e5ba1e013acbd4a22f
Harshit Srivastava <srivastavar025@gmail.com>
2021-05-10 16:46:51 +0530

    aliases

--- Index.js
+++ Index.js
@@ -279,7 +279,7 @@ bot.on('message', async message => {
     } 
     
   }
-  let cmdexe = bot.commands.has(command) || bot.commands.find(c=>c.aliases&&c.aliases.includes(command));
+  let cmdexe = bot.commands.get(command) || bot.commands.find(c=>c.aliases&&c.aliases.includes(command));
   if (!cmdexe) return;
   bot.commands.get(cmdexe).execute(message, args, bot, Discord, prefix);
 });
