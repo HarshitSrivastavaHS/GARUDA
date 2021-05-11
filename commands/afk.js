21a08367bee5e7ae24c8a966de29069bcd267b28
Harshit Srivastava <srivastavar025@gmail.com>
2021-05-11 20:00:20 +0530

    afk mentions fixed

--- commands/afk.js
+++ commands/afk.js
@@ -17,9 +17,11 @@ module.exports = {
         missingPerms = missingPerms.join("\n");
         if (botPerms.includes(false)) return message.channel.send(`The Following permissions which are missing are needed by the bot for this command:\n\n\`\`\`\n${missingPerms.replace("_"," ")}\`\`\``).catch(err=>console.log(`Missing send message permission in a server.`));
         let afkmsg = args.length<1?"AFK":args.join(" ");
-        if (afkmsg.includes("@everyone")||afkmsg.includes("@here")||(/<@[0-9]+>/g).test(afkmsg))
-        bot.afk.set(message.author.id, afkmsg);
-        message.reply(`AFK successfully set, msg: ${afkmsg}`);
+        if (afkmsg.includes("@everyone")||afkmsg.includes("@here")||(/<@[0-9]+>/g).test(afkmsg)) return message.channel.send("AFK messages cannot contain everyone, here or role mentions.")
+        message.reply(`AFK successfully set, msg: ${afkmsg}\nIt Would start working after 15 seconds :D`);
+        setTimeout(()=>{
+                bot.afk.set(message.author.id, afkmsg);
+        }, 15000)
         
         await mongo().then(async (mongoose)=>{
           
