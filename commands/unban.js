063875f452af4cef8d09bf7cdca7b921f38489d1
TechAllByHarshit <srivastavar025@gmail.com~>
2021-05-26 21:11:18 +0530

    unban

--- commands/unban.js
+++ commands/unban.js
@@ -18,30 +18,23 @@ module.exports = {
         if (!message.member.permissions.has("BAN_MEMBERS")) return message.channel.send("You don't have the required permissions.");
             
             if (!args) return message.channel.send("Please also mention the id of the person who is to be unbanned");
-            let err = false;
-            message.guild.fetchBans().then((bans)=>{
-                if (bans.size==0) {
-                    err = true;
-                    message.channel.send("Nobody is banned from this server.");
-                }
-                let user = bans.filter(b=>b.user.id==args[0])
-                if (!user) {
-                    err = true;
-                    message.channel.send("That user isn't banned.");
-                }
-            }).catch(()=>{
-                err = true;
-                return message.channel.send("I don't have the required permissions.");
-            })
             
-            if (!err) {
+            let bans = await message.guild.fetchBans();
 
+            if (!bans) return message.channel.send("I don't have the required permission");
+
+            if (bans.size == 0) return message.channel.send("Nobody is banned");
+
+            let ban = bans.filter(b=>b.user.id == args[0]);
+
+            if (!ban) return message.channel.send("That user is not banned");
+            
             message.guild.members.unban(args[0]).then((u)=>{
                 message.channel.send(`Successfully unbanned ${u.tag}`);
             }).catch(()=>{
                 message.channel.send("I could not unban that person");
             })
-        }
+        
     }
 }
 
