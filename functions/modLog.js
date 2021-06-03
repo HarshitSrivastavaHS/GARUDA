87bb5f0785929bd82d552fcaddbf1c7a1c712f16
TechAllByHarshit <srivastavar025@gmail.com~>
2021-06-03 21:43:49 +0530

    mod log with delete messages

--- functions/modLog.js
+++ functions/modLog.js
@@ -1,7 +1,7 @@
  module.exports = async (bot, Discord) => {
 
   bot.on("messageDelete", (message)=>{
-    let ml = bot.serverConfig.get(message.guild.id)?bot.serverConfig.get(message.guild.id).ml:undefined;
+    let ml = bot.serverConfig.get(message.guild.id)?bot.serverConfig.get(message.guild.id).modLog:undefined;
 	  if (!ml) return; 
 		let modChannel = message.guild.channels.cache.get(ml);
 		if (!ml) return;
