b008a9543935d44efff460076cca70a2972583ff
TechAllByHarshit <srivastavar025@gmail.com~>
2021-06-12 21:09:49 +0530

    membercount added

---
+++ commands/membercount.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'membercount',
+    type: 'info',
+    usage: '&{prefix}membercount',
+    description: 'shows the total member of people in the server',
+    aliases: ["members"],
+    permissions: ['SEND_MESSAGES'],
+    async execute(message, args, bot, Discord, prefix) {
+        let emb = new Discord.MessageEmbed()
+        .setTitle(`Member Count`)
+        .setColor("#FFA500")
+        .setThumbnail(message.guild.icon?message.guild.iconURL({dynamic: true}):undefined)
+        .setDescription(message.guild.memberCount)
+        .setTimestamp();
+        message.channel.send(emb);
+    }
+}
