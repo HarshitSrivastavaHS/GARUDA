33b27fc459b1fa9315238b6851409d552f9c7573
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-07-04 22:02:29 +0530

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -7,7 +7,7 @@ module.exports = {
     permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
         let upperlimit = 6;
-        if (args[0]&&isNaN(upperlimit)) return message.reply("Invalid argument");
+        if (args[0]&&isNaN(args[0])) return message.reply("Invalid argument");
         if (args[0]&&(args[0]>1024||args[0]<2)) return message.channel.send("Argument provided should be between 2 and 1024");
         if (args[0]) upperlimit = args[0];
         let emb = new Discord.MessageEmbed()
