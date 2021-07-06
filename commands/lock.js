239f0fffc57371a17a135eb596aa9aaef2eb51be
TechAllByHarshit <srivastavar025@gmail.com~>
2021-07-06 21:42:12 +0530

    lock

--- commands/lock.js
+++ commands/lock.js
@@ -7,12 +7,13 @@ module.exports = {
     permissions: ['SEND_MESSAGES', 'MANAGE_GUILD'],
     async execute(message, args, bot, Discord, prefix) {
 	channel = message.channel;
-	if (args[0]&&args[0].includes(message.mentions.channels.first().id))
+	if (args[0]&&message.mentions.channels.first()&&args[0].includes(message.mentions.channels.first().id))
 	    channel = message.mentions.channels.first;
 	if(!channel.permissionsFor(message.member).has("MANAGE_CHANNEL")) {
             message.channel.send("You don't have the required permissions.");
             return;
         }
+	if (!channel.permissionFor(channel.guild.roles.everyone).has("SEND_MESSAGES")) return message.channel.send(`${channel} is already locked`);
 	channel.updateOverwrite(channel.guild.roles.everyone, { SEND_MESSAGES: false });
 	args = args.slice(1);
 	message.channel.send(`Successfully locked ${channel}`);
