abe6b6395e9dcaf8bf2df3f0a5c71c4171cbd3a9
TechAllByHarshit <srivastavar025@gmail.com~>
2021-07-11 21:50:39 +0530

    lol

--- commands/icon.js
+++ commands/icon.js
@@ -7,8 +7,8 @@ module.exports = {
     async execute(message, args, bot, Discord, prefix) {
         const embed = new Discord.MessageEmbed()
         .setColor("#D441EE")
-        .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL({dynamic: true})}`);
-        avataremb.setImage(`${message.guild.iconURL({size: 4096, dynamic: true})}`)
+        .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL({dynamic: true})}`)
+        .setImage(`${message.guild.iconURL({size: 4096, dynamic: true})}`)
         .setTitle(`${message.guild.name}'s Icon`);
         message.channel.send(embed);
     }
