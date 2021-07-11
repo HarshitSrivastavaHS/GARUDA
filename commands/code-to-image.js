2c3c72143b9fd34bbc9316e3385f5e2593260c45
TechAllByHarshit <srivastavar025@gmail.com~>
2021-07-11 21:59:07 +0530

    code to image command updated

--- commands/code-to-image.js
+++ commands/code-to-image.js
@@ -10,10 +10,10 @@ module.exports = {
 		message.channel.send("Please wait");
 		let text =  args.join(" ").match(/```[^```]+```|[\\S]+```[^```]+/);
 		if (text) {
-			text = text[0].trim();
+			text = text.trim();
 			text = text.replace(/^```|```$|\n```$/g, "")
-			let space = text.indexOf(" ")
-			let newLine = text.indexOf("\n")
+			let space = text.indexOf(/ /)
+			let newLine = text.indexOf(/\n/)
 			if (space > newLine || space == -1 && newLine != -1)
 				text = text.split("\n").slice(1).join("\n").trim();
       		}
@@ -23,33 +23,38 @@ Example:
 %code-to-image \`\`\`
 hello world
 \`\`\``
-		const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-        	const page = await browser.newPage();
-        	await page.goto('https://carbon.now.sh');
-        	for (let i = 1; i<=16; i++)
-            		await page.keyboard.press('Tab');    
-        	await page.keyboard.down('Control');
-        	await page.keyboard.press('a');
-        	await page.keyboard.up('Control');
-        	await page.keyboard.press("Backspace");
-        	await page.keyboard.type(text);
-		await page.click('.settings-container')
-		await page.click('.preset-container')
-		for (let i = 1; i<=2; i++)
-			await page.keyboard.press("Tab");
-		await page.keyboard.press("Enter");
-		await page.click(".settings-container");
-		const handler = await page.$('.handler');
-		const bounding_box = await handler.boundingBox();
-
-		await page.mouse.move(bounding_box.x + bounding_box.width / 2, bounding_box.y + bounding_box.height / 2);
-		await page.mouse.down();
-		await page.mouse.move(bounding_box.x+bounding_box.width/2+10, bounding_box.y+bounding_box.height/2+10);
-		await page.mouse.up();
-		await page.click("h2.mt3")
-        	const logo = await page.$('[id="export-container"]');        // logo is the element you want to capture
-        	let x = await logo.screenshot();
-        	message.channel.send(new Discord.MessageAttachment(x, "code.png"));
-        	await browser.close();
+// 		const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+//         	const page = await browser.newPage();
+//         	await page.goto('https://carbon.now.sh');
+//         	for (let i = 1; i<=16; i++)
+//             		await page.keyboard.press('Tab');    
+//         	await page.keyboard.down('Control');
+//         	await page.keyboard.press('a');
+//         	await page.keyboard.up('Control');
+//         	await page.keyboard.press("Backspace");
+//         	await page.keyboard.type(text);
+// 		await page.click('.settings-container')
+// 		await page.click('.preset-container')
+// 		for (let i = 1; i<=2; i++)
+// 			await page.keyboard.press("Tab");
+// 		await page.keyboard.press("Enter");
+// 		await page.click(".settings-container");
+// 		const handler = await page.$('.handler');
+// 		const bounding_box = await handler.boundingBox();
+// 
+// 		await page.mouse.move(bounding_box.x + bounding_box.width / 2, bounding_box.y + bounding_box.height / 2);
+// 		await page.mouse.down();
+// 		await page.mouse.move(bounding_box.x+bounding_box.width/2+10, bounding_box.y+bounding_box.height/2+10);
+// 		await page.mouse.up();
+// 		await page.click("h2.mt3")
+//         	const logo = await page.$('[id="export-container"]');        // logo is the element you want to capture
+//         	let x = await logo.screenshot();
+//         	await browser.close();
+        const createCTI = codeSnippet => {
+            let y = codeSnippet.replace(/ /g,"%20").replace(/\n/g,"%250a");
+            const burl = `https://carbonnowsh.herokuapp.com/?code=${y}`;
+            return burl
+        }
+        message.channel.send(new Discord.MessageAttachment(createCTI(text), "code.png"));
 	}
 }
