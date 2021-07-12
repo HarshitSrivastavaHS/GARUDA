8f9afc9f946079d2ce2cb4382f735138c633c351
TechAllByHarshit <srivastavar025@gmail.com~>
2021-07-12 12:52:33 +0530

    .

--- commands/membercount.js
+++ commands/membercount.js
@@ -6,14 +6,13 @@ module.exports = {
     aliases: ["members"],
     permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
-        message.guild.members.fetch();
+        await message.guild.members.fetch();
         let emb = new Discord.MessageEmbed()
         .setTitle(`Member Count`)
         .setColor("#FFA500")
         .setThumbnail(message.guild.icon?message.guild.iconURL({dynamic: true}):undefined)
         .setDescription(`Total members: ${message.guild.members.cache.size}\nTotal Humans: ${message.guild.members.cache.filter(m=>!m.user.bot).size}\nTotal bots: ${message.guild.members.cache.filter(m=>m.user.bot).size}`)
         .setTimestamp();
-        message.guild.members.cache.delete();
         message.channel.send(emb);
     }
 }
