293a2fd8b33abd29978621b02fd1b2811bb07d10
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-07-20 20:43:41 +0530

    Update code-to-image.js

--- commands/code-to-image.js
+++ commands/code-to-image.js
@@ -53,8 +53,8 @@ console.log(hi);`
 //         	await browser.close();
         const createCTI = codeSnippet => {
             //let y = codeSnippet.replace(/%/g,"%25").replace(/ /g,"%20").replace(/\n/g,"%250a").replace(/\+/g,"%2B");
-	    let y = urlencode(codeSnippet, "gbk");
-	    //let y = encodeURIComponent(codeSnippet)
+	    //let y = urlencode(codeSnippet, "gbk");
+	    let y = encodeURIComponent(codeSnippet)
             const burl = `https://carbonnowsh.herokuapp.com/?code=${y}`;
             return burl
         }
