0960fa830c9b14c1db5e3a01f482caae8f318864
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-07-25 10:05:45 +0530

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -11,7 +11,7 @@ module.exports = {
         if (afkmsg.includes("@everyone")||afkmsg.includes("@here")||(/<@&[0-9]+>/g).test(afkmsg)) return message.channel.send("AFK messages cannot contain everyone, here or role mentions.")
         message.reply(`AFK successfully set, msg: ${afkmsg}\nIt Would start working after 15 seconds :D`);
         setTimeout(()=>{
-                bot.afk.set(message.author.id, {msg: afkmsg, time: Math.floor(messgae.createdTimestamp/1000-100)});
+                bot.afk.set(message.author.id, {msg: afkmsg, time: Math.floor(message.createdTimestamp/1000)});
         }, 15000)
         
         await mongo().then(async (mongoose)=>{
@@ -21,7 +21,7 @@ module.exports = {
                 },{
                     _id: message.author.id,
                     afk: afkmsg,
-                    time: Math.floor(message.createdTimestamp/1000-100)
+                    time: Math.floor(message.createdTimestamp/1000)
                 },{
                     upsert: true
                 })
