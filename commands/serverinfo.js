b6cbdef06da3e771b1811f6869f5ae5655f61fa0
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-08-06 14:50:26 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,14 +7,16 @@ module.exports = {
     permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
         await message.guild.members.fetch();
+        let veri = message.guild.verificationLevel;
+        let reg = message.guild.region;
         let emb = new Discord.MessageEmbed()
         .setTitle(`${message.guild.name}'s Server Information`)
         .setColor("#FFA500")
         .addField("General", `✹ Owner: ${message.guild.owner} (${message.guild.owner.user.tag})\n\
 ✹ Created on: ${message.guild.createdAt.toLocaleString("en-IN",{dateStyle: "long"})}\n\
-✹ Server Region: ${message.guild.region}\n\
+✹ Server Region: ${reg[0].toUpperCase+reg.substr(1).toLowerCase().replace("_"," ")}\n\
 ✹ Verified Server: ${message.guild.verified?"Yes":"No"}\n\
-✹ Verification Level: ${message.guild.verificationLevel}`)
+✹ Verification Level: ${veri[0].toUpperCase+veri.substr(1).toLowerCase().replace("_"," ")}`)
         .addField("Categories & Channels", `✹ Categories: ${message.guild.channels.cache.filter(c=>c.type=="category").size}\n\
 ✹ Text: ${message.guild.channels.cache.filter(c=>c.type=="text").size}\n\
 ✹ Voice Channels: ${message.guild.channels.cache.filter(c=>c.type=="voice").size}\n\
