963ab33d31dfa2dad7d49e32e66178367dbf2ecc
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-08-06 14:52:24 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -14,9 +14,9 @@ module.exports = {
         .setColor("#FFA500")
         .addField("General", `✹ Owner: ${message.guild.owner} (${message.guild.owner.user.tag})\n\
 ✹ Created on: ${message.guild.createdAt.toLocaleString("en-IN",{dateStyle: "long"})}\n\
-✹ Server Region: ${reg[0].toUpperCase+reg.substr(1).toLowerCase().replace("_"," ")}\n\
+✹ Server Region: ${reg[0].toUpperCase()+reg.substr(1).toLowerCase().replace("_"," ")}\n\
 ✹ Verified Server: ${message.guild.verified?"Yes":"No"}\n\
-✹ Verification Level: ${veri[0].toUpperCase+veri.substr(1).toLowerCase().replace("_"," ")}`)
+✹ Verification Level: ${veri[0].toUpperCase()+veri.substr(1).toLowerCase().replace("_"," ")}`)
         .addField("Categories & Channels", `✹ Categories: ${message.guild.channels.cache.filter(c=>c.type=="category").size}\n\
 ✹ Text: ${message.guild.channels.cache.filter(c=>c.type=="text").size}\n\
 ✹ Voice Channels: ${message.guild.channels.cache.filter(c=>c.type=="voice").size}\n\
