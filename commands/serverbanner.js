367d815a7cc781bfdc3b168f9f08bde315e1f7cb
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-08-06 21:21:37 +0530

    Create serverbanner.js

---
+++ commands/serverbanner.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'server-banner',
+    description: 'shows the server icon',
+    usage: "&{prefix}server-banner",
+    permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
+    aliases: ["serverbanner", "sb"],
+    async execute(message, args, bot, Discord, prefix) {
+        const embed = new Discord.MessageEmbed()
+        .setColor("#D441EE")
+        .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL({dynamic: true})}`)
+        .setImage(`${message.guild.bannerURL({size: 4096, dynamic: true})}`)
+        .setTitle(`${message.guild.name}'s Icon`);
+        message.channel.send(embed);
+    }
+}
