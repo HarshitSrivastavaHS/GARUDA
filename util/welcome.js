c8a2832db48addf4f270e33e4842fc468bc85f06
TechAllByHarshit <srivastavar025@gmail.com~>
2021-08-07 21:31:07 +0530

    oof

--- util/welcome.js
+++ util/welcome.js
@@ -1,42 +1,60 @@
-const Canvas = require("canvas");
+const Canvas = require("discord-canvas")
 
 module.exports = {
     async execute(member, welcomeCH, Discord) {
-      if (member.guild.members.cache.size!=member.guild.memberCount) 
-        await member.guild.members.fetch();
-      const canvas = Canvas.createCanvas(700, 250);
-      const ctx = canvas.getContext("2d");
-      const background = await Canvas.loadImage('./util/welcome.png');
-      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
-      ctx.fillRect(0, 0, 700, 250);
-      ctx.strokeStyle = "#FFFFFF";
-      ctx.lineWidth = 10;
-      ctx.strokeRect(0, 0, canvas.width, canvas.height);
-      let text = `Welcome to ${member.guild.name}`;
-      let fontSize = 50;
-      do {
-		    ctx.font = `bold ${fontSize -= 1}px sans-serif`;
-	    } while (ctx.measureText(text).width > canvas.width - 260);
-	    ctx.fillStyle = '#edf2f4';
-	    ctx.fillText(text, 225, canvas.height / 3.5);
-      fontSize = 50;
-      text = member.user.tag;
-      do {
-		    ctx.font = `bold ${fontSize -= 1}px sans-serif`;
-	    } while (ctx.measureText(text).width > canvas.width - 300);
-	    ctx.fillStyle = '#edf2f4';
-	    ctx.fillText(text, 250, canvas.height / 1.8);
-      ctx.font = `bold 28px sans-serif`;
-      ctx.fillText(`Humans: ${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`, 240+220-ctx.measureText(`Member #${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`).width/2, canvas.height / 3.5 + canvas.height / 1.8 - 10);
-      ctx.beginPath();
-	    ctx.arc(125, 125, 75, 0, Math.PI * 2, true);
-    	ctx.closePath();
-      ctx.stroke(); 
-	    ctx.clip();
-      const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-      ctx.drawImage(avatar, 50, 50, 150, 150);
-      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');  
+      await member.guild.members.fetch();
+      // const canvas = Canvas.createCanvas(700, 250);
+      // const ctx = canvas.getContext("2d");
+      // const background = await Canvas.loadImage('./util/welcome.png');
+      // ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+      // ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+      // ctx.fillRect(0, 0, 700, 250);
+      // ctx.strokeStyle = "#FFFFFF";
+      // ctx.lineWidth = 10;
+      // ctx.strokeRect(0, 0, canvas.width, canvas.height);
+      // let text = `Welcome to ${member.guild.name}`;
+      // let fontSize = 50;
+      // do {
+		  //   ctx.font = `bold ${fontSize -= 1}px sans-serif`;
+	    // } while (ctx.measureText(text).width > canvas.width - 260);
+	    // ctx.fillStyle = '#edf2f4';
+	    // ctx.fillText(text, 225, canvas.height / 3.5);
+      // fontSize = 50;
+      // text = member.user.tag;
+      // do {
+		  //   ctx.font = `bold ${fontSize -= 1}px sans-serif`;
+	    // } while (ctx.measureText(text).width > canvas.width - 300);
+	    // ctx.fillStyle = '#edf2f4';
+	    // ctx.fillText(text, 250, canvas.height / 1.8);
+      // ctx.font = `bold 28px sans-serif`;
+      // ctx.fillText(`Humans: ${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`, 240+220-ctx.measureText(`Member #${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`).width/2, canvas.height / 3.5 + canvas.height / 1.8 - 10);
+      // ctx.beginPath();
+	    // ctx.arc(125, 125, 75, 0, Math.PI * 2, true);
+    	// ctx.closePath();
+      // ctx.stroke(); 
+	    // ctx.clip();
+      // const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+      // ctx.drawImage(avatar, 50, 50, 150, 150);
+      // const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');  
+
+      const image = await new Canvas.Goodbye()
+        .setUsername(member.user.username)
+        .setDiscriminator(member.user.discriminator)
+        .setMemberCount(message.guild.members.cache.filter(m=>!m.user.bot).size)
+        .setGuildName(member.guild.name)
+        .setAvatar(member.user.displayAvatarURL())
+        .setColor("border", "#8015EA")
+        .setColor("username-box", "#8015EA")
+        .setColor("discriminator-box", "#8015EA")
+        .setColor("message-box", "#8015EA")
+        .setColor("title", "#8015EA")
+        .setColor("avatar", "#8015EA")
+        .setBackground("https://cdn.craftburg.net/stockage/img/discord/background.jpg")
+        .toAttachment();
+
+      const attachment = new Discord.Attachment(image.toBuffer(), "welcome-image.png");
+
+      
       welcomeCH.send(`Welcome to **${member.guild.name}**, <@${member.user.id}>`,attachment);
     }
 }
