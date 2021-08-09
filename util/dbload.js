8c3c422da3584f454fd780de88a0dfb230f23b8a
TechAllByHarshit <srivastavar025@gmail.com~>
2021-08-09 20:26:52 +0530

    v13

---
+++ util/dbload.js
@@ -0,0 +1,72 @@
+const mongo = require(`../mongo`);
+const serverConfig = require('../Schemas/server-config');
+const afkConfig = require('../Schemas/afk');
+const freezerConfig = require('../Schemas/freezenick');
+const giveawaySchema = require('../Schemas/giveaway-schema.js');
+const give = require('../functions/giveaway.js');
+module.exports = async (bot) => {
+
+    //loads per server settings
+    bot.serverConfig = new Map();
+    const server = (async ()=>{
+        const results = await serverConfig.find();
+        for (const result of results){
+            bot.serverConfig.set(result._id, {
+                prefix: result.prefix,
+                suggestion: result.suggestion,
+                welcome: result.welcome,
+                leave: result.leave,
+                modLog: result.modLog,
+                ghost: result.ghost,
+                autoRole: result.autoRole,
+                goal: result.goal
+            });
+        }
+    })();
+
+    //loads afk data
+    bot.afk = new Map();
+    const afkusers = (async ()=>{
+	    const results = await afkConfig.find();
+        for (const result of results){
+            bot.afk.set(result._id, {msg: result.afk, time: result.time});
+        }
+    })();
+
+    //loads all freezed nick users
+    bot.freezer = new Map();
+    const freezedusers = (async ()=>{
+	    const results = await freezerConfig.find();
+        for (const result of results){
+            bot.freezer.set(result._id, result.nick);
+        }
+    })();
+
+    //loads all the giveaways
+    let allGiveaways;
+	await mongo().then(async mongoose => {
+			allGiveaways = await giveawaySchema.find({});
+	});
+	if (allGiveaways.length >= 1) {
+        for (let x in allDocuments) {
+            give(
+                bot,
+                Discord,
+                allDocuments[x]._id,
+                allDocuments[x].endTime,
+                allDocuments[x].winners,
+                allDocuments[x].prize,
+                allDocuments[x].chID,
+                allDocuments[x].host,
+                allDocuments[x].reqs,
+                false
+            );
+        }
+    }
+
+	
+
+}
+
+
+  
