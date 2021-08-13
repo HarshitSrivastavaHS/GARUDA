9f259fd7bc624e38e1a93760e735446881d187df
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-08-13 20:34:32 +0530

    Update ghostping.js

--- functions/ghostping.js
+++ functions/ghostping.js
@@ -1,6 +1,7 @@
  module.exports = async (bot, Discord) => {
     bot.on("messageDelete", async (message) => {
         if (message.author.bot) return;
+	if (message.content == "") return;
         let ghost = bot.serverConfig.get(message.guild.id)?bot.serverConfig.get(message.guild.id).ghost:undefined;
 	    if (!ghost) return; 
 		let mention = message.mentions.members.filter(m=>!m.user.bot&&m.user.id!=message.author.id).first() || message.mentions.roles.first() || message.mentions.everyone;
