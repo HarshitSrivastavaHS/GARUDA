ae546793ec26bd012a58d029e89fdc34810277dc
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-08-17 17:47:32 +0530

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -10,7 +10,7 @@ module.exports = {
         let suggestionchannel = bot.serverConfig.get(message.guild.id)!=undefined?bot.serverConfig.get(message.guild.id).suggestion:undefined;
         if (!suggestionchannel) return mes.edit(`No suggestion channel set. If you are an administrator, please do \`${prefix}help setsuggestion\` to know how to set a suggestion channel.`)
       
-        let channel = message.guild.fetch(suggestionchannel);
+        let channel = await message.guild.fetch(suggestionchannel);
         let botPerms = [];
         let missingPerms = [];
         this.permissions.forEach(p=>{
