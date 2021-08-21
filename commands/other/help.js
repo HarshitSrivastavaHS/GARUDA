f7c11104eee2d5e169ed4c07ece5f0b3e0f0b25b
TechAllByHarshit <srivastavar025@gmail.com~>
2021-08-21 21:28:13 +0530

    help command remake

--- commands/other/help.js
+++ commands/other/help.js
@@ -10,7 +10,7 @@ module.exports = {
             return message.channel.send("Command is disabled due to some bugs")
         const PREFIX_REG = /&{prefix}/g;
         const fs = require('fs');
-        const categories = fs.readdirSync(`./commands/`);
+        const categories = fs.readdirSync(`../../commands/`);
         let cmd = bot.commands.get(args[0]?args[0].toLowerCase():"") || bot.commands.find(c=>c.aliases&&c.aliases.includes(args[0]?args[0].toLowerCase():""));
         let ctg = categories.find(c=>c==args[0]?.toLowerCase())
         const helpembed = new Discord.MessageEmbed()    
@@ -24,18 +24,22 @@ module.exports = {
            helpembed.addFields({name:`Name`, value: `${command.name}`},{name:`Description`, value: `${command.description}`},{name:`Usage`, value: `${command.usage?command.usage.replace(PREFIX_REG, prefix):"not added"}`}, {name:`Aliases`, value: `${command.aliases.length>0?command.aliases.join(", "):"No Alias"}`}, {name:`Permissions Required by bot`, value: `${command.permissions?command.permissions.join(", ").toLowerCase().replace(/_/g," "):"not added"}`});
         }
         else if (ctg) {
-            let cat = require(`./commands/${ctg}`);
+            let cat = fs.readdirSync(`../../commands/${ctg}/`).filter(f=>f.endsWith(".js"));
+            for (let file in cat){
+                file = require(`../../commands/${cat}/${file}`)
+                str += `\`\`${file.name}\`\` `;
+            }
             helpembed.addFields({
-                name: `Name`,
-                value: `${cat.join(", ")}`
+                name: `Commands`,
+                value: `${str}`
             })
         }
         else {
             helpembed.setDescription(`Do \`${prefix}help <command>\` for more info on that command.\nJoin the support server: https://discord.gg/sBe3jNSdqN`);
             for (let category of categories) {
                 let str = "";
-                category = require(`./commands/${category}`)
-                for (const file of category) {
+                let commands = fs.readdirSync(`./commands/${category}/`).filter(f=>f.endsWith(".js"));
+                for (const file of commands) {
                     const command = require(`./commands/${category}/${file}`);
                     str += `\`${command.name}\``;
                 }
