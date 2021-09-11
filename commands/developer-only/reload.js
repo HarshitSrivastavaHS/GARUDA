6d1e7288c114a81c9c04d75e6246fc019b2dc4be
TechAllByHarshit <srivastavar025@gmail.com~>
2021-09-11 21:55:01 +0530

    RE:load

--- commands/developer-only/reload.js
+++ commands/developer-only/reload.js
@@ -11,27 +11,25 @@ module.exports = {
         if (args){
             fs.readdir("./commands", (err, categories)=>{
                 if (err) return message.reply(`Error:\`\`\`js\n${err}\`\`\``)
-                console.log(categories);
                 if (!args[0]) return message.reply("Please type the name of the category")
                 if (!categories.includes(args[0].toLowerCase())) return message.reply(`Category \`${args[0]}\` not found`);
                 let index = categories.indexOf(args[0]);
                 let cmd;
-                console.log(categories[index]);
-//                 if (!args[1]) {
-//                     cmd = fs.readdirSync(`./commands/${categories[index]}/`).filter(f=>f.endsWith(".js"))
-//                     for (let command of cmd) {
-//                         command = require(`./../../commands/${categories[index]}/${command}`)
-//                         bot.commands.delete(command.name)
-//                         bot.commands.set(command.name, {categories[index], command, aliases: command.aliases})
-//                     }
-//                 }
-//                 else {
-//                     cmd = fs.readdirSync(`./commands/${categories[index]}/`).filter(f=>f == args[1].toLowerCase());
-//                     if (!cmd) return message.reply(`Command \`${args[1]}\` not found in category: \`${args[0]}\``);
-//                         command = require(`./../../commands/${categories[index]}/${cmd}`);
-//                         bot.commands.delete(command.name)
-//                         bot.commands.set(command.name, {categories[index], command, aliases: command.aliases});
-//                 }
+                if (!args[1]) {
+                    cmd = fs.readdirSync(`./commands/${categories[index]}/`).filter(f=>f.endsWith(".js"))
+                    for (let command of cmd) {
+                        command = require(`./../../commands/${categories[index]}/${command}`)
+                        bot.commands.delete(command.name)
+                        bot.commands.set(command.name, {categories[index], command, aliases: command.aliases})
+                    }
+                }
+                else {
+                    cmd = fs.readdirSync(`./commands/${categories[index]}/`).filter(f=>f == args[1].toLowerCase());
+                    if (!cmd) return message.reply(`Command \`${args[1]}\` not found in category: \`${args[0]}\``);
+                        command = require(`./../../commands/${categories[index]}/${cmd}`);
+                        bot.commands.delete(command.name)
+                        bot.commands.set(command.name, {categories[index], command, aliases: command.aliases});
+                }
             });
         }
     }
