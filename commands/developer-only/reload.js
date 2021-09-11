84f6c7dc7359aa5cd91d00706a319342cc4da341
TechAllByHarshit <srivastavar025@gmail.com~>
2021-09-11 21:11:04 +0530

    a push

---
+++ commands/developer-only/reload.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: "reload"
+    async execute(message, args) {
+        if (message.author.id != "451693463742840842") return message.reply("Not a public command.");
+        if (!args) return message.reply("Which command do i have to reload?");
+        if (args){
+            fs.readdir("./commands/", (err, categories)=>{
+                if (err) return message.reply(`Error:\n /`/`/`js\n${err}/`/`/``)
+                if (!categories.include(args[0].toLowerCase())) return message.reply(`Category \`${args[0]}\` not found`);
+                let index = categories.indexOf(args[0]);
+                let cmd;
+                if (!args[1]) {
+                    cmd = fs.readdirSync(`./commands/${categories[index]}/`).filter(f=>f.endsWith(".js"))
+                    for (let command of cmd) {
+                        command = require(`./commands/${categories[index]}/${command}`)
+                        bot.commands.set(command.name, {categories[index], command, aliases: command.aliases})
+                    }
+                }
+                else {
+                    cmd = fs.readdirSync(`./commands/${categories[index]}/`).filter(f=>f == args[1].toLowerCase());
+                    if (!cmd) return message.reply(`Command \`${args[1]}\` not found in category: \`${args[0]}\``);
+                        command = require(`./commands/${categories[index]}/${cmd}`)
+                        bot.commands.set(command.name, {categories[index], command, aliases: command.aliases});
+                }
+            });
+        }
+    }
+}
