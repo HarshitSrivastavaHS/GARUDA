27dc76b382817945375abaaa46621a20a7d0a0c2
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-09-14 19:51:29 +0530

    Update glist.js

--- commands/giveaway/glist.js
+++ commands/giveaway/glist.js
@@ -6,7 +6,7 @@ module.exports = {
     permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
         if (message.author.id != "451693463742840842") return messasge.reply("in development");
-        let ong = bot.giveaways.get(message.guild.id)!=undefined?bot.giveaways.get(message.guild.id):[];
+        /*let ong = bot.giveaways.get(message.guild.id)!=undefined?bot.giveaways.get(message.guild.id):[];
         if (ong.length == 0) return message.reply("No ongoing giveaways in this server");
         let emb = new Discord.MessageEmbed();
         let desc = "Giveaways going on in this server:";
@@ -18,6 +18,6 @@ module.exports = {
         .setColor("GREEN")
         .setTitle("Giveaways")
         .setThumbnail(bot.user.displayAvatarURL());
-        message.reply({embeds: [emb]});
+        message.reply({embeds: [emb]});*/
     }
 }
