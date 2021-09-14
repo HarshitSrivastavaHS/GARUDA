7237fd23d530a6b3ca52c4134975415443bf180c
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-09-14 19:51:08 +0530

    Update dbload.js

--- util/dbload.js
+++ util/dbload.js
@@ -44,15 +44,15 @@ module.exports = async (bot) => {
     })();
 
     //loads all the giveaways
-    bot.giveaways = new Map();
+    //bot.giveaways = new Map();
     let allDocuments;
 	await mongo().then(async mongoose => {
 			allDocuments = await giveawaySchema.find({});
 	});
 	if (allDocuments.length >= 1) {
         for (let x in allDocuments) {
-            let ong = bot.giveaways.get(allDocuments[x].guild)!=undefined?bot.giveaways.get(allDocuments[x].guild):[];
-            bot.giveaways.set(allDocuments[x].guild, ong.push([allDocuments[x]._id, allDocuments[x].guild, allDocuments[x].chID, allDocuments[x].prize]));
+            /*let ong = bot.giveaways.get(allDocuments[x].guild)!=undefined?bot.giveaways.get(allDocuments[x].guild):[];
+            bot.giveaways.set(allDocuments[x].guild, ong.push([allDocuments[x]._id, allDocuments[x].guild, allDocuments[x].chID, allDocuments[x].prize]));*/
             give(
                 bot,
                 Discord,
