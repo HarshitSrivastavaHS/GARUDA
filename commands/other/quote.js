957bf6872193933221607420e7ddb1643163e5fc
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-10-06 15:28:46 +0530

    Create quote.js

---
+++ commands/other/quote.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'quote',
+    usage: '&{prefix}quote',
+    description: 'get a random quote',
+    aliases: ["thought", "quotes"]
+    permissions: ['SEND_MESSAGES'],
+    async execute(message, args, bot, Discord, prefix) {
+        
+        message.channel.sendTyping();
+        const fetch = require("node-fetch");
+        try {
+            fetch(`http://www.famous-quotes.uk/api.php?id=random`).then((res)=>{
+                return res.json()
+            }).then ((data)=>{
+                message.channel.send(`**${data[0][1]}** - ${data[0][2]}`);
+            }).catch(err=>{message.channel.send("💔 Something went wrong");})
+        }
+         catch(err){
+                message.channel.send("💔 Something went wrong");
+        }
+
+    }
+}
