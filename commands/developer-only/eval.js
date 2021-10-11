12c2f3161a488861d959a3abfa6517e1cc802876
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-10-11 14:02:31 +0530

    Update and rename reload.js to eval.js

---
+++ commands/developer-only/eval.js
@@ -0,0 +1,30 @@
+const fs = require("fs")
+module.exports = {
+    name: "eval",
+    usage: '&{prefix}eval',
+    description: '¯\_(ツ)_/¯',
+    aliases: [],
+    permissions: ['SEND_MESSAGES'],
+    async execute(message, args, bot, Discord, prefix) {
+        if (message.author.id != "451693463742840842") return message.reply("Not a public command.");
+    function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+    try {
+         const code = args.join(" ");
+      	let evaled = eval(code);
+	if (evaled instanceof Promise || (Boolean(evaled) && typeof evaled.then === 'function' && typeof evaled.catch === 'function')) evaled = await evaled;
+      	if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled, { depth: 0 });
+
+
+      return message.channel.send({ content: clean(evaled), code: true, split: true });
+    } catch (err) {
+      return message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    } 
+    
+    }
+}
