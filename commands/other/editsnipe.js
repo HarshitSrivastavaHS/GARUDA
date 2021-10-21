c73b3ddfd5da65551c13af44763a5b8a964c1b9a
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-10-21 10:30:04 +0530

    Update editsnipe.js

--- commands/other/editsnipe.js
+++ commands/other/editsnipe.js
@@ -12,8 +12,8 @@ module.exports = {
         .setColor("#00dfad")
         .setAuthor(msg.author, msg.avatar)
         .addFields(
-        {name: "Old Message" ,value: msg.oldContent},
-        {name: "New Message" ,value: msg.newContent}
+        {name: "Old Message" ,value: msg.oldContent!=""?msg.oldContent:"[NA]"},
+        {name: "New Message" ,value: msg.newContent!=""?msg.newContent:"[NA]"}
         )
         //.setDescription(`Message Edited on: <t:${Math.floor(msg.time/1000)}:f>`)
         .setFooter(`Message Edited`)
