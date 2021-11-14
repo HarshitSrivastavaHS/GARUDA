78c8a4aeeabe9f15496ba24c881c0132a1e94fb6
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-11-14 10:50:13 +0530

    Update vote.js

--- commands/other/vote.js
+++ commands/other/vote.js
@@ -10,7 +10,9 @@ module.exports = {
        const emb = new Discord.MessageEmbed()
         .setColor("PURPLE")
         .setTitle("Vote for GARUDA")
-        .setDescription("Thank you for using this command. You can [vote for Garuda on Discord Bot List](https://discordbotlist.com/bots/garuda/upvote).\nYou can vote for the bot every 12 hours!.")
+        .setDescription("Thank you for using this command. You can vote for Garuda on Top.gg and Discord Bot List.\nYou can vote for the bot every 12 hours!.")
+        .addField(`**Top.gg**`, `**[Vote Now!](https://top.gg/bot/777840690515279872/vote)**`)
+        .addField(`**Discord Bot List**`, `**[Vote Now!](https://discordbotlist.com/bots/garuda/upvote)**`)
         .setThumbnail(message.author.displayAvatarURL())
         .setTimestamp()
         .setFooter("Thank You!");
