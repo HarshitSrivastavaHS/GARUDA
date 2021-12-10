6565a1a8137b02cdf8dc497a842c4a7a28da79d0
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-12-10 13:18:14 +0530

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,29 +1,10 @@
 module.exports = {
-    name: "interactionCreate",
-    async execute(interaction) {
-        let bot = interaction.client;
+  name: "interactionCreate",
+  async execute(interaction){
+    let bot = interaction.client;
 
-        if (!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return;
 
-        bot.commands.get(interaction.commandName.toLowerCase()).command.slashExecute(interaction);
-        const partners = [
-            {
-                name: "Dumbot",
-                linkName: "Bot Invite",
-                link: "https://discord.com/oauth2/authorize?client_id=870239976690970625&permissions=0&scope=bot"
-            }
-        ];
-        if (Math.random() * 50 == 2) {
-            const partnershipEmbed = new Discord.MessageEmbed()
-                .setColor("#e52165")
-                .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL({ dynamic: true })}`)
-                .setTitle("Our Partners")
-                .setFooter("Bot by TechAllByHarshit#1503")
-                .setTimestamp();
-            for (let partner of partners) {
-                partnershipEmbed.addField(`${partner.name}`, `[${partner.linkName}](${partner.link})`);
-            }
-            message.reply({ embeds: [partnershipEmbed] });
-        }
-    }
+    bot.commands.get(interaction.commandName.toLowerCase()).command.slashExecute(interaction);
+  }
 }
