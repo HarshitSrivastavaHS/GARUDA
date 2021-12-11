6c77a609af32dbd5f3f131af87c01375b65b969b
Harshit Srivastava <41013322+TechAllByHarshit@users.noreply.github.com>
2021-12-11 21:40:49 +0530

    Update partners.js

--- commands/other/partners.js
+++ commands/other/partners.js
@@ -14,7 +14,7 @@ module.exports = {
         },
         {
           name: "Dumbot",
-          linkName: "Bot Invite",
+          linkName: "Bot Invite (Admin)",
           link: "https://discord.com/api/oauth2/authorize?client_id=919244474029334548&permissions=8&scope=applications.commands%20bot"
         }
       ];
