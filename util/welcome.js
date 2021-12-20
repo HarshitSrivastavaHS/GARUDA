aa138debe37a90763d23a1ad6414a4031d155917
TechAllByHarshit <=>
2021-12-21 00:47:09 +0530

    Added multiple images for welcomer :D

--- util/welcome.js
+++ util/welcome.js
@@ -3,45 +3,12 @@ const Canvas = require("discord-canvas")
 module.exports = {
     async execute(member, welcomeCH, Discord) {
       await member.guild.members.fetch();
-      // const canvas = Canvas.createCanvas(700, 250);
-      // const ctx = canvas.getContext("2d");
-      // const background = await Canvas.loadImage('./util/welcome.png');
-      // ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-      // ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
-      // ctx.fillRect(0, 0, 700, 250);
-      // ctx.strokeStyle = "#FFFFFF";
-      // ctx.lineWidth = 10;
-      // ctx.strokeRect(0, 0, canvas.width, canvas.height);
-      // let text = `Welcome to ${member.guild.name}`;
-      // let fontSize = 50;
-      // do {
-		  //   ctx.font = `bold ${fontSize -= 1}px sans-serif`;
-	    // } while (ctx.measureText(text).width > canvas.width - 260);
-	    // ctx.fillStyle = '#edf2f4';
-	    // ctx.fillText(text, 225, canvas.height / 3.5);
-      // fontSize = 50;
-      // text = member.user.tag;
-      // do {
-		  //   ctx.font = `bold ${fontSize -= 1}px sans-serif`;
-	    // } while (ctx.measureText(text).width > canvas.width - 300);
-	    // ctx.fillStyle = '#edf2f4';
-	    // ctx.fillText(text, 250, canvas.height / 1.8);
-      // ctx.font = `bold 28px sans-serif`;
-      // ctx.fillText(`Humans: ${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`, 240+220-ctx.measureText(`Member #${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`).width/2, canvas.height / 3.5 + canvas.height / 1.8 - 10);
-      // ctx.beginPath();
-	    // ctx.arc(125, 125, 75, 0, Math.PI * 2, true);
-    	// ctx.closePath();
-      // ctx.stroke(); 
-	    // ctx.clip();
-      // const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-      // ctx.drawImage(avatar, 50, 50, 150, 150);
-      // const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');  
-
+      let random = Math.floor(Math.random()*6+1);
       const image = await new Canvas.Welcome()
         .setUsername(member.user.username)
         .setDiscriminator(member.user.discriminator)
         .setGuildName(member.guild.name)
-        .setAvatar(member.user.displayAvatarURL({format: "jpg"}))
+        .setAvatar(member.user.displayAvatarURL({format: "jpg", size: 4096}))
         .setText("message", "Welcome to {server}")
         .setText("member-count", `Humans: ${member.guild.members.cache.filter(m=>!m.user.bot).size} Bots: ${member.guild.members.cache.filter(m=>m.user.bot).size}`)
         .setColor("border", "#8015EA")
@@ -50,7 +17,7 @@ module.exports = {
         .setColor("message-box", "#8015EA")
         .setColor("title", "#8015EA")
         .setColor("avatar", "#8015EA")
-        .setBackground("util/welcome.jpg")
+        .setBackground(`util/welcome${random}.jpg`)
         .toAttachment();
         
       const attachment = new Discord.MessageAttachment(image.toBuffer(), "welcome-image.png");
