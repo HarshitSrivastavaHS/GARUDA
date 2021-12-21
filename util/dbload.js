32253e6dcf7d2c0cc29ce5305060808d2e62496b
TechAllByHarshit <=>
2021-12-21 23:12:43 +0530

    dbload command returned to normal

--- util/dbload.js
+++ util/dbload.js
@@ -23,19 +23,6 @@ module.exports = async (bot) => {
                 goal: result.goal,
                 giveaway: result.giveawayManagers
             });
-            await mongo().then(async (mongoose)=>{
-                
-                await serverConfig.findOneAndUpdate({
-                    _id: result._id
-                    },{
-                        _id: result._id,
-                        welcomer: {
-                            channel: result.welcome, 
-                        },
-                    },{
-                        upsert: true
-                    })
-            });
         }
     })();
 
