8a5d9e1d0b6c6b2677ddcef7ca494b56c3c7d054
TechAllByHarshit <=>
2021-12-21 22:59:49 +0530

    1 file connected for db

---
+++ functions/serverConfig.js
@@ -0,0 +1,17 @@
+const mongo = require(`../mongo`);
+const serverConfig = require('../Schemas/server-config');
+
+module.exports = (async (bot, guild)=>{
+    let result = await serverConfig.findOne(guild.id);
+    bot.serverConfig.set(result._id, {
+        prefix: result.prefix,
+        suggestion: result.suggestion,
+        welcome: result.welcome,
+        leave: result.leave,
+        modLog: result.modLog,
+        ghost: result.ghost,
+        autoRole: result.autoRole,
+        goal: result.goal,
+        giveaway: result.giveawayManagers
+    });
+})
