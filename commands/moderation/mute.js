3f293b7dc90da40ce1e2555432851e5f7659a952
TechAllByHarshit <=>
2021-12-25 01:40:44 +0530

    mute/unmute command added

---
+++ commands/moderation/mute.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+module.exports = {
+    name: 'mute',
+    description: 'mute a member.',
+    usage: '&{prefix}mute <@user> <time> (reason)',
+    aliases: ["timeout"],
+    permissions: ['SEND_MESSAGES', "READ_MESSAGE_HISTORY","MODERATE_MEMBERS" ],
+    async execute(message, args, bot, Discord, prefix) {
+        if (message.author.id != "451693463742840842") return message.reply("In development");
+        if (!message.member.permissions.has("MODERATE_MEMBERS")) return message.reply("You don't have the permission to mute others.")
+    
+        const mentionMember = message.mentions.members.first()&&message.mentions.members.filter(m=>args[0]&&args[0].includes(m.user.id)).size>=1?message.mentions.members.filter(m=>args[0]&&args[0].includes(m.user.id)).first(): undefined;
+        if (!mentionMember) return message.reply("Please mention the member to be muted.");
+        if (mentionMember.user.id == message.author.id) return message.reply("You cannot mute yourself.");
+        if (mentionMember.user.id == bot.user.id) return message.reply("I cannot mute myself");
+        if (mentionMember.communicationDisabledUntilTimestamp != null) return message.reply("The person is already muted.");
+
+        let time = undefined;
+        if (args[1]) 
+            time = ms(args[1])
+        if (!time) return message.reply("Please mention the duration of the mute.");
+
+        args.splice(0,2);
+        let reason = args.join(" ");
+
+        const srole = message.member.roles.highest.position;
+        const rrole = mentionMember.roles.highest.position;
+        const brole = message.guild.me.roles.highest.position;
+        let owner = await message.guild.fetchOwner();
+        if (srole>rrole || owner.id == message.author.id) {
+            if (brole>rrole) {
+                mentionMember.timeout(time, reason);
+                message.channel.send(`${mentionMember} was muted by ${message.member} for ${ms(time, { long: true })}.`);
+            }
+            else {
+                message.reply("My role is not high enough to mute them. Please move my role higher than their highest role..");
+            }
+        }
+        else {
+            message.reply("You cannot mute someone who has the same highest role as you.");
+        }
+
+    }
+}
---
+++ commands/moderation/unmute.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+module.exports = {
+    name: 'unmute',
+    description: 'mute a member.',
+    usage: '&{prefix}unmute <@user>',
+    aliases: ["timeout-remove"],
+    permissions: ['SEND_MESSAGES', "READ_MESSAGE_HISTORY","MODERATE_MEMBERS" ],
+    async execute(message, args, bot, Discord, prefix) {
+        if (message.author.id != "451693463742840842") return message.reply("In development");
+        if (!message.member.permissions.has("MODERATE_MEMBERS")) return message.reply("You don't have the permission to mute others.")
+    
+        const mentionMember = message.mentions.members.first()&&message.mentions.members.filter(m=>args[0]&&args[0].includes(m.user.id)).size>=1?message.mentions.members.filter(m=>args[0]&&args[0].includes(m.user.id)).first(): undefined;
+        if (!mentionMember) return message.reply("Please mention the member to be muted.");
+        if (mentionMember.user.id == message.author.id) return message.reply("You are not muted.");
+        if (mentionMember.user.id == bot.user.id) return message.reply("I am not muted");
+        if (mentionMember.communicationDisabledUntilTimestamp==null) return message.reply("The person is not muted.");
+
+        const srole = message.member.roles.highest.position;
+        const rrole = mentionMember.roles.highest.position;
+        const brole = message.guild.me.roles.highest.position;
+        let owner = await message.guild.fetchOwner();
+        if (srole>rrole || owner.id == message.author.id) {
+            if (brole>rrole) {
+                mentionMember.timeout(null);
+                message.channel.send(`${mentionMember} was unmuted by ${message.member}`);
+            }
+            else {
+                message.reply("My role is not high enough to unmute them. Please move my role higher than their highest role..");
+            }
+        }
+        else {
+            message.reply("You cannot unmute someone who has the same highest role as you.");
+        }
+
+    }
+}
