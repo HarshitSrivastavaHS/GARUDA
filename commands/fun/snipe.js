bc342966c9a9451c480007d4761451707705cac3
TechAllByHarshit <=>
2022-04-02 15:10:11 +0530

    snipe slash command added

--- commands/fun/snipe.js
+++ commands/fun/snipe.js
@@ -1,21 +1,38 @@
-module.exports = {
+const Discord = require("discord.js")
+const { SlashCommandBuilder } = require('@discordjs/builders');
+async function main(bot, channel) {
+    let msg = bot.snipes.get(channel);
+    if (msg) { 
+        let snipEmbed = new Discord.MessageEmbed()
+            .setColor("#ff2052")
+            .setAuthor({name: `${msg.author}`, iconURL: msg.avatar})
+            .setDescription(msg.content)
+            .setImage(msg.image?msg.image:null)
+            .setFooter({text: `Message Deleted`})
+            .setTimestamp(msg.time);
+        return snipEmbed;
+    }
+    else {
+        return (
+            new Discord.MessageEmbed()
+                .setColor("#00dfad")
+                .setDescription("No deleted message in this channel.")
+                .setTimestamp()
+            );
+    }
+}module.exports = {
     name: 'snipe',
     
     description: 'shows last deleted message in that channel',
     aliases: [],
     permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
-        
-        let msg = bot.snipes.get(message.channel.id);
-        if (!msg) return message.channel.send("There is no deleted message in this channel.");
-        let snipEMbed = new Discord.MessageEmbed()
-        .setColor("#ff2052")
-        .setAuthor(msg.author, msg.avatar)
-        .setDescription(msg.content)
-        .setImage(msg.image?msg.image:null)
-        //addField("Message Deleted on", `<t:${Math.floor(msg.time/1000)}:f>`)
-        .setFooter("Message Deleted")
-        .setTimestamp(msg.time)
-        message.channel.send({embeds:[snipEMbed]});
+       message.channel.send({embeds: [await main(bot, message.channel.id)]});
+    },
+    slash: new SlashCommandBuilder()
+        .setName("snipe")
+        .setDescription("See the last deleted message in that channel."),
+    async slashExecute(interaction) {
+        interaction.reply({embeds:[await main(interaction.client,interaction.channel.id)]})
     }
 }
