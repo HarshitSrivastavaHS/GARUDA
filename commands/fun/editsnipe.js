3a7ed4caf2ef0f80a5973934c538f59c5e328998
TechAllByHarshit <=>
2022-04-02 15:06:12 +0530

    editsnipe slash command added

--- commands/fun/editsnipe.js
+++ commands/fun/editsnipe.js
@@ -1,23 +1,46 @@
+const Discord = require("discord.js")
+const { SlashCommandBuilder } = require('@discordjs/builders');
+async function main(bot, channel) {
+    let msg = bot.editSnipes.get(channel);
+    if (msg) { 
+        let editSnipEMbed = new Discord.MessageEmbed()
+            .setColor("#00dfad")
+            .setAuthor({name: `${msg.author}`, iconURL: msg.avatar})
+            .addFields(
+                {
+                    name: "Old Message" ,value: msg.oldContent!=""?msg.oldContent:"[NA]"
+                },
+                {
+                    name: "New Message" ,value: msg.newContent!=""?msg.newContent:"[NA]"
+                }
+            )
+            .setFooter({text: `Message Edited`})
+            .setTimestamp(msg.time);
+        return editSnipEMbed;
+    }
+    else {
+        return (
+            new Discord.MessageEmbed()
+                .setColor("#00dfad")
+                .setDescription("No edited message in this channel.")
+                .setTimestamp()
+            );
+    }
+}
 module.exports = {
     name: 'editsnipe',
-    
     usage: '&{prefix}editsnipe',
     description: 'shows last edited message in that channel',
     aliases: [],
     permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
     async execute(message, args, bot, Discord, prefix) {
-       let msg = bot.editSnipes.get(message.channel.id);
-        if (!msg) return message.channel.send("There is no edited message in this channel.");
-        let editSnipEMbed = new Discord.MessageEmbed()
-        .setColor("#00dfad")
-        .setAuthor(msg.author, msg.avatar)
-        .addFields(
-        {name: "Old Message" ,value: msg.oldContent!=""?msg.oldContent:"[NA]"},
-        {name: "New Message" ,value: msg.newContent!=""?msg.newContent:"[NA]"}
-        )
-        //.setDescription(`Message Edited on: <t:${Math.floor(msg.time/1000)}:f>`)
-        .setFooter(`Message Edited`)
-        .setTimestamp(msg.time);
-        message.channel.send({embeds: [editSnipEMbed]});
+       
+        message.channel.send({embeds: [await main(bot, message.channel.id)]});
+    },
+    slash: new SlashCommandBuilder()
+        .setName("editsnipe")
+        .setDescription("See the last edited message in that channel."),
+    async slashExecute(interaction) {
+        interaction.reply({embeds:[await main(interaction.client,interaction.channel.id)]})
     }
 }
