dc81fdce5550c35f4e8cf5a7fb68e272f3003127
TechAllByHarshit <=>
2022-04-02 14:39:30 +0530

    ascii slash command added

--- commands/fun/ascii.js
+++ commands/fun/ascii.js
@@ -1,19 +1,30 @@
 const figlet = require("util").promisify(require("figlet"));
+const Discord = require("discord.js")
+const { SlashCommandBuilder } = require('@discordjs/builders');
+async function main(text) {
+  let banner = (await figlet(text)).substring(0,1900);
+  banner = Discord.Formatters.codeBlock("md", banner);
+  return banner;
+}
 module.exports = {
     name: 'ascii',
-    
     usage: '&{prefix}banner <text>',
     description: 'types that text in banner form',
     aliases: [],
     permissions: ['SEND_MESSAGES'],
     async execute(message, args, bot, Discord, prefix) {
       if (args.length==0) return message.channel.send("Please enter the text.");
-      let banner = await figlet(args.join("\n"));
-      if (banner.length>2000) {
-        message.channel.send("The banner is greater than discord message limit :(")
-        return;
-      }
-      banner = Discord.Formatters.codeBlock("md", banner)
-      message.channel.send(banner, {code : true})
+      message.channel.send(await main(args.join(" ")));
+    },
+    slash: new SlashCommandBuilder()
+      .setName('ascii')
+      .setDescription('Types that text in cool looking form')
+      .addStringOption(option =>
+	    	option.setName('text')
+			    .setDescription('Your Text')
+          .setRequired(true)),
+    async slashExecute(interaction) {
+      let text = interaction.options.getString("text");
+      interaction.reply(await main(text));
     }
 }
